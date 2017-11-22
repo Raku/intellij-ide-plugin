52431d2f58a4ab606f453002a658fec26ee6813e
Altai-man <altay-man@mail.ru>
2017-11-22 15:00:06 +0200

    Make icon final

--- src/edument/perl6idea/Perl6Icons.java
+++ src/edument/perl6idea/Perl6Icons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class Perl6Icons {
-    public static Icon CAMELIA = IconLoader.getIcon("/icons/camelia.png");
+    public static final Icon CAMELIA = IconLoader.getIcon("/icons/camelia.png");
 }
