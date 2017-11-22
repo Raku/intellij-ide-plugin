b263470e65e9c449f0ab29ac7590728999f48a70
Altai-man <altay-man@mail.ru>
2017-11-22 15:26:18 +0200

    Type does not need to know about steps.

--- src/edument/perl6idea/module/Perl6ModuleType.java
+++ src/edument/perl6idea/module/Perl6ModuleType.java
@@ -1,15 +1,9 @@
 package edument.perl6idea.module;
 
-import com.intellij.ide.util.projectWizard.ModuleBuilder;
-import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.module.ModuleTypeManager;
-import com.intellij.openapi.projectRoots.SdkTypeId;
-import com.intellij.openapi.util.Condition;
 import edument.perl6idea.Perl6Icons;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
@@ -46,11 +40,5 @@ public class Perl6ModuleType extends ModuleType<Perl6ModuleBuilder> {
     @Override
     public Icon getNodeIcon(@Deprecated boolean b) {
         return Perl6Icons.CAMELIA;
-    }
-
-    @Nullable
-    public ModuleWizardStep modifyProjectTypeStep(@NotNull SettingsStep settingsStep, @NotNull final ModuleBuilder moduleBuilder) {
-        final Condition<SdkTypeId> condition = moduleBuilder::isSuitableSdkType;
-        return new Perl6SettingsStep(settingsStep, moduleBuilder, condition);
     }
 }
