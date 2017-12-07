719d44a96faefa035327b7e1afc98ef50f34762e
Jonathan Worthington <jnthn@jnthn.net>
2017-12-07 12:32:13 +0100

    Add generated Perl6ElementTypes file

---
+++ src/edument/perl6idea/parsing/Perl6ElementTypes.java
@@ -0,0 +1,11 @@
+package edument.perl6idea.parsing;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.IFileElementType;
+import edument.perl6idea.Perl6Language;
+
+public interface Perl6ElementTypes {
+    IElementType STATEMENT = new Perl6ElementType("STATEMENT");
+    IElementType USE_STATEMENT = new Perl6ElementType("USE_STATEMENT");
+    IElementType STATEMENT_LIST = new Perl6ElementType("STATEMENT_LIST");
+    IFileElementType FILE = new IFileElementType(Perl6Language.INSTANCE);
+}
