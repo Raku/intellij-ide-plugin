59b1ce043fe2ac6218f10d8215f84d4d8466b93e
Altai-man <altay-man@mail.ru>
2017-12-20 20:31:29 +0200

    Show passed, failed and skipped tests

--- src/edument/perl6idea/testing/TapOutputToGeneralTestEventsConverter.java
+++ src/edument/perl6idea/testing/TapOutputToGeneralTestEventsConverter.java
@@ -11,6 +11,8 @@ import org.tap4j.consumer.TapConsumer;
 import org.tap4j.consumer.TapConsumerFactory;
 import org.tap4j.model.TestResult;
 import org.tap4j.model.TestSet;
+import org.tap4j.util.DirectiveValues;
+import org.tap4j.util.StatusValues;
 
 import java.text.ParseException;
 import java.util.List;
@@ -44,19 +46,44 @@ public class TapOutputToGeneralTestEventsConverter extends OutputToGeneralTestEv
         currentTap = "";
         List<TestResult> results = set.getTestResults();
         super.processServiceMessages(new ServiceMessageBuilder("enteredTheMatrix").toString(), ProcessOutputTypes.STDOUT, myVisitor);
-        ServiceMessageBuilder countBuilder = new ServiceMessageBuilder("testCount")
-                .addAttribute("count", String.valueOf(set.getNumberOfTestResults()));
-        super.processServiceMessages(countBuilder.toString(), ProcessOutputTypes.STDOUT, myVisitor);
+        processTestsCount(set);
+        processSingleSuite(results);
+    }
+
+    private void processSingleSuite(List<TestResult> results) throws ParseException {
         super.processServiceMessages(ServiceMessageBuilder.testSuiteStarted("testSuite").toString(), ProcessOutputTypes.STDOUT, myVisitor);
-        for (int i = 0; i < results.size(); i++) {
-            ServiceMessageBuilder testStarted = ServiceMessageBuilder.testStarted("testSuite.perl6.test.test" + i);
-            super.processServiceMessages(testStarted.toString(), ProcessOutputTypes.STDOUT, myVisitor);
-            ServiceMessageBuilder text = ServiceMessageBuilder.testStdOut("testSuite.perl6.test.test" + i);
-            text.addAttribute("out", "text");
-            super.processServiceMessages(text.toString(), ProcessOutputTypes.STDOUT, myVisitor);
-            ServiceMessageBuilder testFinished = ServiceMessageBuilder.testFinished("testSuite.perl6.test.test" + i);
-            super.processServiceMessages(testFinished.toString(), ProcessOutputTypes.STDOUT, myVisitor);
+        for (TestResult result : results) {
+            processSingleTest(result);
         }
         super.processServiceMessages(ServiceMessageBuilder.testSuiteFinished("testSuite").toString(), ProcessOutputTypes.STDOUT, myVisitor);
+    }
+
+    private void processTestsCount(TestSet set) throws ParseException {
+        String message = new ServiceMessageBuilder("testCount")
+                .addAttribute("count", String.valueOf(set.getNumberOfTestResults())).toString();
+        handleMessageSend(message);
+    }
+
+    private void processSingleTest(TestResult testResult) throws ParseException {
+        String testName = String.format("%d %s", testResult.getTestNumber(), testResult.getDescription());
+        handleMessageSend(ServiceMessageBuilder.testStarted(testName).toString());
+        if (testResult.getStatus() == StatusValues.OK &&
+            testResult.getDirective() == null) {
+        } else if (testResult.getDirective().getDirectiveValue() == DirectiveValues.SKIP ||
+                   testResult.getDirective().getDirectiveValue() == DirectiveValues.TODO) {
+            String message = ServiceMessageBuilder.testIgnored(testName)
+                    .addAttribute("message", String.format("%s %s", testName, testResult.getDirective().getReason())).toString();
+            handleMessageSend(message);
+        } else if (testResult.getStatus() == StatusValues.NOT_OK) {
+            String message = ServiceMessageBuilder.testFailed(testName)
+                    .addAttribute("error", "true")
+                    .addAttribute("message", testResult.getDescription()).toString();
+            handleMessageSend(message);
+        }
+        handleMessageSend(ServiceMessageBuilder.testFinished(testName).toString());
+    }
+
+    private void handleMessageSend(String message) throws ParseException {
+        super.processServiceMessages(message, ProcessOutputTypes.STDOUT, myVisitor);
     }
 }
