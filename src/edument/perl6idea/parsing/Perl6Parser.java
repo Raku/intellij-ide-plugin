9cd23f4c335c566801dbcf8b2f025f8e8b3e3492
Jonathan Worthington <jnthn@jnthn.net>
2017-12-23 17:44:05 +0100

    Add generated stub Perl 6 parser

---
+++ src/edument/perl6idea/parsing/Perl6Parser.java
@@ -0,0 +1,16 @@
+package edument.perl6idea.parsing;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.lang.PsiBuilder;
+import com.intellij.lang.PsiParser;
+
+public class Perl6Parser implements PsiParser {
+
+    public ASTNode parse(IElementType root, PsiBuilder builder) {
+        PsiBuilder.Marker rootMarker;
+        rootMarker = builder.mark();
+        rootMarker.done(root);
+        return builder.getTreeBuilt();
+    }
+
+}
