dea145064c06ace775c0ac7432e4247786f2e819
Altai-man <altay-man@mail.ru>
2018-01-23 12:02:59 +0200

    Remove hardcoded paths

--- src/edument/perl6idea/debugger/Perl6DebugCommandLineState.java
+++ src/edument/perl6idea/debugger/Perl6DebugCommandLineState.java
@@ -13,13 +13,19 @@ public class Perl6DebugCommandLineState extends Perl6RunCommandLineState {
         populateRunCommand();
     }
 
-    private void populateRunCommand() {
-        // TODO Remove hardcoded paths ASAP
-        this.command.add("/home/koto/Work/CustomPerl6/MoarVM/install/bin/moar");
+    private void populateRunCommand() throws ExecutionException {
+        String moar_path = System.getenv("DEBUG_MOAR_PATH");
+        String libpath =  System.getenv("DEBUG_LIBPATH_PATH");
+        String nqp = System.getenv("DEBUG_NQP_PATH");
+        if (moar_path == null || libpath == null || nqp == null) {
+            System.out.println("Variables DEBUG_MOAR_PATH, DEBUG_LIBPATH_PATH and DEBUG_NQP_PATH must be set!");
+            throw new ExecutionException("Variables DEBUG_MOAR_PATH, DEBUG_LIBPATH_PATH and DEBUG_NQP_PATH must be set!");
+        }
+        this.command.add(moar_path);
         this.command.add("--debug-port=9999");
         // TODO Make suspend on start is optional
         this.command.add("--debug-suspend");
-        this.command.add("--libpath=/home/koto/Work/CustomPerl6/nqp/install/lib");
-        this.command.add("/usr/share/nqp/lib/nqp.moarvm");
+        this.command.add("--libpath=" + libpath);
+        this.command.add(nqp);
     }
 }
