34f14936f424dc225afd4eddd0163458af02e5b1
Altai-man <altay-man@mail.ru>
2018-01-23 22:19:12 +0200

    Set up source positions

--- src/edument/perl6idea/debugger/Perl6StackFrame.java
+++ src/edument/perl6idea/debugger/Perl6StackFrame.java
@@ -1,25 +1,47 @@
 package edument.perl6idea.debugger;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.AtomicNullableLazyValue;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.ColoredTextContainer;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.frame.XStackFrame;
+import com.intellij.xdebugger.impl.XSourcePositionImpl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public class Perl6StackFrame extends XStackFrame {
     private final Perl6StackFrameDescriptor myFrameDescriptor;
     private final Perl6ExecutionStack myStack;
     private final Perl6DebugThread myDebugThread;
+    private final AtomicNullableLazyValue<VirtualFile> myVirtualFile;
 
     public Perl6StackFrame(Perl6StackFrameDescriptor frame, Perl6ExecutionStack stack) {
         myFrameDescriptor = frame;
         myStack = stack;
         myDebugThread = stack.getSuspendContext().getDebugThread();
+        myVirtualFile = AtomicNullableLazyValue.createValue(() -> {
+            String localFilePath = myFrameDescriptor.getFile().getPath();
+            return VfsUtil.findFileByIoFile(new File(localFilePath), true);
+        });
     }
 
     @Override
     public void customizePresentation(@NotNull ColoredTextContainer component) {
         component.append(myFrameDescriptor.getPresentableName(), SimpleTextAttributes.REGULAR_ATTRIBUTES);
         component.setIcon(AllIcons.Debugger.StackFrame);
+    }
+
+    @Nullable
+    @Override
+    public XSourcePosition getSourcePosition() {
+        VirtualFile file = myVirtualFile.getValue();
+        if (file != null)
+            return XSourcePositionImpl.create(file, myFrameDescriptor.getLine() - 1);
+        return null;
     }
 }
--- src/edument/perl6idea/debugger/Perl6StackFrameDescriptor.java
+++ src/edument/perl6idea/debugger/Perl6StackFrameDescriptor.java
@@ -37,4 +37,8 @@ public class Perl6StackFrameDescriptor {
             return bytecodeFile + ":" + line;
         }
     }
+
+    public int getLine() {
+        return line;
+    }
 }
