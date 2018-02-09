2af2f46b36a833317cdd3b384196ca558732628a
Jonathan Worthington <jnthn@jnthn.net>
2018-02-09 01:47:53 +0100

    Add QuoteRegex PSI classes missed in last commit

---
+++ src/edument/perl6idea/psi/impl/Perl6QuoteRegexImpl.java
@@ -0,0 +1,12 @@
+package edument.perl6idea.psi.impl;
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.lang.ASTNode;
+import edument.perl6idea.psi.Perl6QuoteRegex;
+import org.jetbrains.annotations.NotNull;
+
+public class Perl6QuoteRegexImpl extends ASTWrapperPsiElement implements Perl6QuoteRegex {
+    public Perl6QuoteRegexImpl(@NotNull ASTNode node) {
+        super(node);
+    }
+}
