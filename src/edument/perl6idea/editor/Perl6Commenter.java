4ca0129a64465a4c43fb6e98697bb6e6da7cc9f2
Altai-man <altay-man@mail.ru>
2018-03-04 20:45:47 +0200

    Implement Commenter

---
+++ src/edument/perl6idea/editor/Perl6Commenter.java
@@ -0,0 +1,36 @@
+package edument.perl6idea.editor;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class Perl6Commenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "#";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
