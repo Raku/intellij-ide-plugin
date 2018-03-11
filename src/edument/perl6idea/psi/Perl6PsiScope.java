923ee7558e55fbb52484f5fcb25267e155a709a5
Jonathan Worthington <jnthn@jnthn.net>
2018-03-11 17:13:09 +0100

    Remove left-behind debugging code

--- src/edument/perl6idea/psi/Perl6PsiScope.java
+++ src/edument/perl6idea/psi/Perl6PsiScope.java
@@ -10,35 +10,24 @@ import java.util.Queue;
 public interface Perl6PsiScope extends Perl6PsiElement {
     default List<Perl6PsiElement> getDeclarations() {
         List<Perl6PsiElement> declarations = new ArrayList<>();
-        System.err.println("Getting declarations of " + this.toString());
-        try {
-            Queue<Perl6PsiElement> visit = new LinkedList<>();
-            visit.add(this);
-            while (!visit.isEmpty()) {
-                Perl6PsiElement current = visit.remove();
-                //System.err.println("Took from queue " + current.toString());
-                boolean addChildren = false;
-                if (current == this) {
-                    addChildren = true;
-                } else {
-                    //System.err.println("Considering child " + current.toString());
-                    if (current instanceof Perl6PsiDeclaration) {
-                        declarations.add(current);
-                        System.err.println("Adding declaration " + current.toString());
-                    }
-                    if (!(current instanceof Perl6PsiScope))
-                        addChildren = true;
+        Queue<Perl6PsiElement> visit = new LinkedList<>();
+        visit.add(this);
+        while (!visit.isEmpty()) {
+            Perl6PsiElement current = visit.remove();
+            boolean addChildren = false;
+            if (current == this) {
+                addChildren = true;
+            } else {
+                if (current instanceof Perl6PsiDeclaration) {
+                    declarations.add(current);
                 }
-                if (addChildren)
-                    for (PsiElement e : current.getChildren())
-                        if (e instanceof Perl6PsiElement) {
-                            //System.err.println("Adding to queue " + e.toString());
-                            visit.add((Perl6PsiElement) e);
-                        }
+                if (!(current instanceof Perl6PsiScope))
+                    addChildren = true;
             }
-        }
-        catch (Throwable t) {
-            System.err.println(t.toString());
+            if (addChildren)
+                for (PsiElement e : current.getChildren())
+                    if (e instanceof Perl6PsiElement)
+                        visit.add((Perl6PsiElement) e);
         }
         return declarations;
     }
