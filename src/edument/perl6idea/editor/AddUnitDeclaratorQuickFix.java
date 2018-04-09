a04b852fa35d479528ae2bf8556ca1577550e446
Timo Paulssen <timonator@perpetuum-immobile.de>
2018-04-09 04:26:29 +0200

    add annotation and quickfix for "unit" usage
    complains about unit + braces and, more importantly,
    about semicolon form without unit keyword.
    
    It offers a quickfix that inserts the "unit" keyword,
    but it doesn't seem to actually do anything if the file
    in question is a scratch file.
    
    Also, only offers the quickfix if the cursor is in
    front of the semicolon. Should ideally offer whatever
    place the cursor is on inside the declaration line
    or something.
    
    Part of P6I-147

---
+++ src/edument/perl6idea/editor/AddUnitDeclaratorQuickFix.java
@@ -0,0 +1,84 @@
+package edument.perl6idea.editor;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.ide.scratch.ScratchFileService;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.util.IncorrectOperationException;
+import edument.perl6idea.parsing.Perl6TokenTypes;
+import edument.perl6idea.psi.Perl6PackageDecl;
+import org.jetbrains.annotations.Nls;import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AddUnitDeclaratorQuickFix extends PsiElementBaseIntentionAction implements IntentionAction {
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement psiElement) throws IncorrectOperationException {
+        PsiElement ourParent = psiElement.getParent();
+        int offset = ourParent.getTextOffset();
+
+        new WriteCommandAction.Simple(project) {
+            @Override
+            protected void run() throws Throwable {
+                try {
+                    FileEditorManager.getInstance(project).openFile(ourParent.getContainingFile().getVirtualFile(), true);
+                    final Editor editor1 = FileEditorManager.getInstance(project).getSelectedTextEditor();
+                    final Document document = editor1.getDocument();
+                    document.insertString(offset, "unit ");
+                }
+                catch (Exception e) {
+                    int bar = 1;
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement psiElement) {
+        if (psiElement.getNode().getElementType() != Perl6TokenTypes.STATEMENT_TERMINATOR) {
+            return false;
+        }
+        if (!(psiElement.getParent() instanceof Perl6PackageDecl)) {
+            return false;
+        }
+        return true;
+    }
+
+    protected boolean checkFile(@Nullable PsiFile file) {
+        if (file == null) {
+            return false;
+        } else {
+            PsiManager manager = file.getManager();
+            if (manager == null) {
+                return false;
+            }
+            if (manager.isInProject(file)) {
+                return true;
+            }
+            if (ScratchFileService.isInScratchRoot(file.getVirtualFile())) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        /* TODO: what should these be? Where do they appear? */
+        return "Family of Comma Intentions";
+    }
+
+    @Override
+    public String getText() {
+        return "Add missing 'unit' scope declaration";
+    }
+}
