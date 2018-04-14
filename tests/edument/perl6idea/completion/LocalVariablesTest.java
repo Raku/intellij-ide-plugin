ccfe6450985367264a03a5e6b921e76cd411018f
Altai-man <altay-man@mail.ru>
2018-04-14 14:59:25 +0300

    Test all sigils

--- tests/edument/perl6idea/completion/LocalVariablesTest.java
+++ tests/edument/perl6idea/completion/LocalVariablesTest.java
@@ -3,6 +3,7 @@ package edument.perl6idea.completion;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,11 +14,32 @@ public class LocalVariablesTest extends LightCodeInsightFixtureTestCase {
     }
 
     public void testCompletion() {
-        myFixture.configureByFile("DefaultTestDataSigil1.pm6");
+        myFixture.configureByFile("DefaultTestDataSigil.pm6");
         myFixture.complete(CompletionType.BASIC, 1);
         List<String> vars = myFixture.getLookupElementStrings();
         assertNotNull(vars);
         assertTrue(vars.containsAll(Collections.singletonList("$a")));
         assertEquals(10, vars.size());
+
+        myFixture.type("a;\n@");
+        myFixture.complete(CompletionType.BASIC, 1);
+        vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Arrays.asList("@b1", "@b2")));
+        assertEquals(2, vars.size());
+
+        myFixture.type("b1;\n%");
+        myFixture.complete(CompletionType.BASIC, 1);
+        vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Arrays.asList("%c1", "%c2")));
+        assertEquals(3, vars.size());
+
+        myFixture.type("c1;\n&");
+        myFixture.complete(CompletionType.BASIC, 1);
+        vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Arrays.asList("&d1", "&d2")));
+        assertEquals(2, vars.size());
     }
 }
