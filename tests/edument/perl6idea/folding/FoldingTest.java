a99741a003cc73c9344518e87055fa42810bd120
Altai-man <altay-man@mail.ru>
2018-04-14 19:45:41 +0300

    Add (failing) folding test

---
+++ tests/edument/perl6idea/folding/FoldingTest.java
@@ -0,0 +1,17 @@
+package edument.perl6idea.folding;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+import java.nio.file.Paths;
+
+public class FoldingTest extends LightCodeInsightFixtureTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return "testData/folding";
+    }
+
+    public void testFolding() {
+        myFixture.configureByFiles("FoldingTestData.p6");
+        myFixture.testFolding(Paths.get(getTestDataPath(), "FoldingTestData.p6").toString());
+    }
+}
