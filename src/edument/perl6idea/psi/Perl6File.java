1c9cd5d7929d332fddca98441ef39f156167225b
Jonathan Worthington <jnthn@jnthn.net>
2018-05-14 15:56:32 +0200

    Perl6File interface should do PsiFile

--- src/edument/perl6idea/psi/Perl6File.java
+++ src/edument/perl6idea/psi/Perl6File.java
@@ -1,6 +1,7 @@
 package edument.perl6idea.psi;
 
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiNameIdentifierOwner;
 
-public interface Perl6File extends Perl6PsiElement, Perl6PsiScope, PsiNameIdentifierOwner {
+public interface Perl6File extends Perl6PsiElement, Perl6PsiScope, PsiNameIdentifierOwner, PsiFile {
 }
