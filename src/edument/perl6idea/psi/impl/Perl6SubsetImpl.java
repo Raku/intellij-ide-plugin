fa8697862d496896d2e619d18407b0b876fb6a40
Altai-man <altay-man@mail.ru>
2018-05-16 18:24:36 +0300

    Make SubsetImpl presentable

--- src/edument/perl6idea/psi/impl/Perl6SubsetImpl.java
+++ src/edument/perl6idea/psi/impl/Perl6SubsetImpl.java
@@ -1,17 +1,16 @@
 package edument.perl6idea.psi.impl;
 
-import com.intellij.extapi.psi.StubBasedPsiElementBase;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import edument.perl6idea.parsing.Perl6ElementTypes;
 import edument.perl6idea.parsing.Perl6TokenTypes;
+import edument.perl6idea.psi.Perl6PresentableStub;
 import edument.perl6idea.psi.Perl6Subset;
-import edument.perl6idea.psi.Perl6SymbolLike;
 import edument.perl6idea.psi.stub.Perl6SubsetStub;
 import edument.perl6idea.psi.stub.Perl6SubsetStubElementType;
 import org.jetbrains.annotations.NotNull;
 
-public class Perl6SubsetImpl extends StubBasedPsiElementBase<Perl6SubsetStub> implements Perl6Subset {
+public class Perl6SubsetImpl extends Perl6PresentableStub<Perl6SubsetStub> implements Perl6Subset {
     public Perl6SubsetImpl(@NotNull ASTNode node) {
         super(node);
     }
