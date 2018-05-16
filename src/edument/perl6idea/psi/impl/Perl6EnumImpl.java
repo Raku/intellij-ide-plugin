35756999992382b8f36b20e77aec3b4431b00971
Altai-man <altay-man@mail.ru>
2018-05-16 19:29:50 +0300

    Simplify code

--- src/edument/perl6idea/psi/impl/Perl6EnumImpl.java
+++ src/edument/perl6idea/psi/impl/Perl6EnumImpl.java
@@ -1,9 +1,6 @@
 package edument.perl6idea.psi.impl;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import edument.perl6idea.parsing.Perl6ElementTypes;
-import edument.perl6idea.parsing.Perl6TokenTypes;
 import edument.perl6idea.psi.Perl6Enum;
 import edument.perl6idea.psi.Perl6PresentableStub;
 import edument.perl6idea.psi.stub.Perl6EnumStub;
@@ -22,11 +19,7 @@ public class Perl6EnumImpl extends Perl6PresentableStub<Perl6EnumStub> implement
     @Override
     public String getEnumName() {
         Perl6EnumStub stub = getStub();
-        if (stub != null)
-            return stub.getEnumName();
-        PsiElement name = findChildByType(Perl6TokenTypes.NAME);
-        PsiElement longName = findChildByType(Perl6ElementTypes.LONG_NAME);
-        return name == null ? longName == null ? "<anon>" : longName.getText() : name.getText();
+        return stub != null ? stub.getEnumName() : getSymbolName();
     }
 
     public String toString() {
--- src/edument/perl6idea/psi/impl/Perl6SubsetImpl.java
+++ src/edument/perl6idea/psi/impl/Perl6SubsetImpl.java
@@ -1,9 +1,6 @@
 package edument.perl6idea.psi.impl;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import edument.perl6idea.parsing.Perl6ElementTypes;
-import edument.perl6idea.parsing.Perl6TokenTypes;
 import edument.perl6idea.psi.Perl6PresentableStub;
 import edument.perl6idea.psi.Perl6Subset;
 import edument.perl6idea.psi.stub.Perl6SubsetStub;
@@ -22,11 +19,7 @@ public class Perl6SubsetImpl extends Perl6PresentableStub<Perl6SubsetStub> imple
     @Override
     public String getSubsetName() {
         Perl6SubsetStub stub = getStub();
-        if (stub != null)
-            return stub.getSubsetName();
-        PsiElement name = findChildByType(Perl6TokenTypes.NAME);
-        PsiElement longName = findChildByType(Perl6ElementTypes.LONG_NAME);
-        return name == null ? longName == null ? "<anon>" : longName.getText() : name.getText();
+        return stub != null ? stub.getSubsetName() : getSymbolName();
     }
 
     public String toString() {
