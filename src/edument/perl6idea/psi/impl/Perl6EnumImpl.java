8ec054cbaa6d9347fd9e82580b55a96a6ce6b338
Altai-man <altay-man@mail.ru>
2018-05-16 19:26:51 +0300

    Enum stub is presentable

--- src/edument/perl6idea/psi/impl/Perl6EnumImpl.java
+++ src/edument/perl6idea/psi/impl/Perl6EnumImpl.java
@@ -1,17 +1,16 @@
 package edument.perl6idea.psi.impl;
 
-import com.intellij.extapi.psi.StubBasedPsiElementBase;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import edument.perl6idea.parsing.Perl6ElementTypes;
 import edument.perl6idea.parsing.Perl6TokenTypes;
 import edument.perl6idea.psi.Perl6Enum;
-import edument.perl6idea.psi.Perl6SymbolLike;
+import edument.perl6idea.psi.Perl6PresentableStub;
 import edument.perl6idea.psi.stub.Perl6EnumStub;
 import edument.perl6idea.psi.stub.Perl6EnumStubElementType;
 import org.jetbrains.annotations.NotNull;
 
-public class Perl6EnumImpl extends StubBasedPsiElementBase<Perl6EnumStub> implements Perl6Enum {
+public class Perl6EnumImpl extends Perl6PresentableStub<Perl6EnumStub> implements Perl6Enum {
     public Perl6EnumImpl(@NotNull ASTNode node) {
         super(node);
     }
