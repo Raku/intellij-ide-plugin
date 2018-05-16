16ee1071f58471fce1cf43e14431070da3409c9d
Altai-man <altay-man@mail.ru>
2018-05-16 21:47:37 +0300

    Fix failure

--- src/edument/perl6idea/psi/Perl6RegexDecl.java
+++ src/edument/perl6idea/psi/Perl6RegexDecl.java
@@ -1,6 +1,10 @@
 package edument.perl6idea.psi;
 
-public interface Perl6RegexDecl extends Perl6PsiElement, Perl6PsiScope, Perl6PsiDeclaration {
+import com.intellij.psi.StubBasedPsiElement;
+import edument.perl6idea.psi.stub.Perl6RegexDeclStub;
+
+public interface Perl6RegexDecl extends Perl6PsiElement, Perl6PsiScope,
+                                        Perl6PsiDeclaration, StubBasedPsiElement<Perl6RegexDeclStub> {
     String getRegexKind();
     String getRegexName();
 }
