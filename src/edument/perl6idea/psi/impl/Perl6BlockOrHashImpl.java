825b0534ba27e3e8f7f84472234c7cf8b9236440
Jonathan Worthington <jnthn@jnthn.net>
2018-05-21 19:00:27 +0200

    Have {...} blocks contribute their implicit $_
    May need a little tweaking, but seems to be about right.

--- src/edument/perl6idea/psi/impl/Perl6BlockOrHashImpl.java
+++ src/edument/perl6idea/psi/impl/Perl6BlockOrHashImpl.java
@@ -2,11 +2,21 @@ package edument.perl6idea.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import edument.perl6idea.psi.Perl6BlockOrHash;
+import edument.perl6idea.psi.Perl6IfStatement;
+import edument.perl6idea.psi.symbols.Perl6ImplicitSymbol;
+import edument.perl6idea.psi.symbols.Perl6SymbolCollector;
+import edument.perl6idea.psi.symbols.Perl6SymbolKind;
 import org.jetbrains.annotations.NotNull;
 
 public class Perl6BlockOrHashImpl extends ASTWrapperPsiElement implements Perl6BlockOrHash {
     public Perl6BlockOrHashImpl(@NotNull ASTNode node) {
         super(node);
+    }
+
+    @Override
+    public void contributeExtraSymbols(Perl6SymbolCollector collector) {
+        collector.offerSymbol(new Perl6ImplicitSymbol(Perl6SymbolKind.Variable, "$_", this));
     }
 }
