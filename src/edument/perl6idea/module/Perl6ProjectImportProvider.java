08917c5e707021f6fc48ef1457bb60673b7274ec
Altai-man <altay-man@mail.ru>
2018-05-21 19:37:47 +0300

    Add needed services and fix a leak by providing a step in project creation sequence. Now we depend on `META6.json` file to import sources.

--- src/edument/perl6idea/module/Perl6ProjectImportProvider.java
+++ src/edument/perl6idea/module/Perl6ProjectImportProvider.java
@@ -1,5 +1,9 @@
 package edument.perl6idea.module;
 
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.ProjectNameStep;
+import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.Nullable;
@@ -9,9 +13,19 @@ public class Perl6ProjectImportProvider extends ProjectImportProvider {
         super(new Perl6ProjectBuilder());
     }
 
+    @Override
+    protected boolean canImportFromFile(VirtualFile file) {
+        return file.getName().equals("META6.json");
+    }
+
+    @Override
+    public ModuleWizardStep[] createSteps(WizardContext context) {
+        return new ModuleWizardStep[]{new ProjectNameStep(context)};
+    }
+
     @Nullable
     @Language("HTML")
     public String getFileSample() {
-        return "<b>Perl 6</b> project file (META6.json";
+        return "<b>Perl 6</b> project file (META6.json)";
     }
 }
