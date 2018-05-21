9b96612e60952e2faea6f1116a8acd1dfdd4a476
Jonathan Worthington <jnthn@jnthn.net>
2018-05-22 00:59:05 +0200

    Simplify declarations search logic
    Which is now only used by StructureView.

--- src/edument/perl6idea/psi/Perl6PsiScope.java
+++ src/edument/perl6idea/psi/Perl6PsiScope.java
@@ -31,8 +31,8 @@ public interface Perl6PsiScope extends Perl6PsiElement {
         return results;
     }
 
-    default List<Object> walkTree(Class<?> type) {
-        List<Object> elements = new ArrayList<>();
+    default List<Perl6PsiDeclaration> getDeclarations() {
+        List<Perl6PsiDeclaration> decls = new ArrayList<>();
         Queue<Perl6PsiElement> visit = new LinkedList<>();
         visit.add(this);
         while (!visit.isEmpty()) {
@@ -41,8 +41,8 @@ public interface Perl6PsiScope extends Perl6PsiElement {
             if (current == this) {
                 addChildren = true;
             } else {
-                if (type.isAssignableFrom(current.getClass()))
-                    elements.add(current);
+                if (current instanceof Perl6PsiDeclaration)
+                    decls.add((Perl6PsiDeclaration)current);
                 if (!(current instanceof Perl6PsiScope))
                     addChildren = true;
             }
@@ -51,14 +51,7 @@ public interface Perl6PsiScope extends Perl6PsiElement {
                     if (e instanceof Perl6PsiElement)
                         visit.add((Perl6PsiElement) e);
         }
-        return elements;
-    }
-
-    default List<Perl6PsiDeclaration> getDeclarations() {
-        List<Object> tree = walkTree(Perl6PsiDeclaration.class);
-        List<Perl6PsiDeclaration> result = new ArrayList<>();
-        for (Object o : tree) { result.add((Perl6PsiDeclaration) o); }
-        return result;
+        return decls;
     }
 
     default void contributeExtraSymbols(Perl6SymbolCollector collector) {
