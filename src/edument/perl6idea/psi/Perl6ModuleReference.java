260c6a12675dd39869dd1e4ae17ddb6c290d6ffb
Jonathan Worthington <jnthn@jnthn.net>
2018-05-22 14:18:03 +0200

    Fix a pair of bugs in module name completion
    * Wrongly indexed the compilation unit name if the file was open
    * Wrognly included modules from other projects. I'm at a loss as to
      why, though the docs do kinda say that getAllKeys may "be out of
      date", which I think is slang for "full of wrong stuff".

--- src/edument/perl6idea/psi/Perl6ModuleReference.java
+++ src/edument/perl6idea/psi/Perl6ModuleReference.java
@@ -5,11 +5,15 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceBase;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndex;
+import edument.perl6idea.psi.stub.index.Perl6StubIndexKeys;
 import edument.perl6idea.psi.stub.index.ProjectModulesStubIndex;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Perl6ModuleReference extends PsiReferenceBase<Perl6PsiElement> {
     public Perl6ModuleReference(@NotNull Perl6ModuleName moduleName) {
@@ -28,8 +32,15 @@ public class Perl6ModuleReference extends PsiReferenceBase<Perl6PsiElement> {
     @NotNull
     @Override
     public Object[] getVariants() {
-        Collection<String> projectModules = ProjectModulesStubIndex.getInstance()
-            .getAllKeys(this.getElement().getProject());
-        return projectModules.toArray();
+        Project project = this.getElement().getProject();
+        Collection<String> projectModules = ProjectModulesStubIndex.getInstance().getAllKeys(project);
+        List<String> reallyInThisProject = new ArrayList<>();
+        for (String module : projectModules) {
+            Collection<Perl6File> matching = ProjectModulesStubIndex.getInstance()
+                .get(module, project, GlobalSearchScope.projectScope(project));
+            if (!matching.isEmpty())
+                reallyInThisProject.add(module);
+        }
+        return reallyInThisProject.toArray();
     }
 }
