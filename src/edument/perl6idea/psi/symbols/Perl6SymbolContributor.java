3b3a924d48e2b2ececf2885eea06d32c46fb32a2
Jonathan Worthington <jnthn@jnthn.net>
2018-05-22 12:14:19 +0200

    Fix regression caused my missing marker interface

--- src/edument/perl6idea/psi/symbols/Perl6SymbolContributor.java
+++ src/edument/perl6idea/psi/symbols/Perl6SymbolContributor.java
@@ -1,5 +1,7 @@
 package edument.perl6idea.psi.symbols;
 
-public interface Perl6SymbolContributor {
+import edument.perl6idea.psi.Perl6PsiElement;
+
+public interface Perl6SymbolContributor extends Perl6PsiElement {
     void contributeSymbols(Perl6SymbolCollector collector);
 }
