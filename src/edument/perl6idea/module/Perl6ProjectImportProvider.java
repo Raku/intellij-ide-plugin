a8f35841b322ba37528b895b90f3e14de50f1eed
Altai-man <altay-man@mail.ru>
2018-05-24 15:05:09 +0300

    Add label for SDK selection step

--- src/edument/perl6idea/module/Perl6ProjectImportProvider.java
+++ src/edument/perl6idea/module/Perl6ProjectImportProvider.java
@@ -8,12 +8,13 @@ import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
+import net.miginfocom.swing.MigLayout;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
+
 public class Perl6ProjectImportProvider extends ProjectImportProvider {
-
-
     protected Perl6ProjectImportProvider() {
         super(new Perl6ProjectBuilder());
     }
@@ -27,7 +28,16 @@ public class Perl6ProjectImportProvider extends ProjectImportProvider {
     public ModuleWizardStep[] createSteps(WizardContext context) {
         final Condition<SdkTypeId> condition = myBuilder::isSuitableSdkType;
         return new ModuleWizardStep[]{new ProjectNameStep(context),
-          new SdkSettingsStep(context, new Perl6ModuleBuilder(), condition, null)};
+          new SdkSettingsStep(context, new Perl6ModuleBuilder(), condition, null) {
+              @Override
+              public JComponent getComponent() {
+                  JComponent oldPanel = super.getComponent();
+                  JPanel newPanel = new JPanel(new MigLayout());
+                  newPanel.add(new JLabel("Select a Perl 6 compiler to use"), "wrap 20");
+                  newPanel.add(oldPanel);
+                  return newPanel;
+              }
+          }};
     }
 
     @Nullable
