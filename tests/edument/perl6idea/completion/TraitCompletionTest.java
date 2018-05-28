8f87a2660aedf1fc5d10f6ec703193e93f956f9c
Altai-man <altay-man@mail.ru>
2018-05-28 17:59:29 +0300

    Add tests

---
+++ tests/edument/perl6idea/completion/TraitCompletionTest.java
@@ -0,0 +1,74 @@
+package edument.perl6idea.completion;
+
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import edument.perl6idea.filetypes.Perl6ScriptFileType;
+import edument.perl6idea.sdk.Perl6SdkType;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TraitCompletionTest extends LightCodeInsightFixtureTestCase {
+    private Sdk testSdk;
+
+    @Override
+    protected String getTestDataPath() {
+        return "testData/codeInsight/localVariables";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ApplicationManager.getApplication().runWriteAction(() -> {
+            String homePath = Perl6SdkType.getInstance().suggestHomePath();
+            assertNotNull("Found a perl6 in path to use in tests", homePath);
+            testSdk = SdkConfigurationUtil.createAndAddSDK(homePath, Perl6SdkType.getInstance());
+            ProjectRootManager.getInstance(myModule.getProject()).setProjectSdk(testSdk);
+        });
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        SdkConfigurationUtil.removeSdk(testSdk);
+        super.tearDown();
+    }
+
+    public void testCompletionForRoutineParameter() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "sub foo($a is <caret>) {}");
+        myFixture.complete(CompletionType.BASIC, 1);
+        List<String> vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Collections.singletonList("rw")));
+        assertEquals(5, vars.size());
+    }
+
+    public void testCompletionForRoutine() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "sub foo is r<caret> {}");
+        myFixture.complete(CompletionType.BASIC, 1);
+        List<String> vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Collections.singletonList("rw")));
+        assertEquals(6, vars.size());
+    }
+
+    public void testCompletionForMultipleTraits() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "sub foo is rw is e<caret> {}");
+        myFixture.complete(CompletionType.BASIC, 1);
+        List<String> vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Collections.singletonList("export")));
+        assertEquals(6, vars.size());
+    }
+
+    public void testCompletionForPackage() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "class Foo is I<foo> {}");
+        myFixture.complete(CompletionType.BASIC, 1);
+        List<String> vars = myFixture.getLookupElementStrings();
+        assertNotNull(vars);
+        assertTrue(vars.containsAll(Collections.singletonList("Int")));
+    }
+}
