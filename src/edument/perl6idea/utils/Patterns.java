995e501698919c0dcb6c8742cfb3118ff271f07c
Altai-man <altay-man@mail.ru>
2018-05-30 21:00:03 +0300

    Liberate my regex (One of me, all of you) We just want to accept names like `foo.p6` too.

--- src/edument/perl6idea/utils/Patterns.java
+++ src/edument/perl6idea/utils/Patterns.java
@@ -3,6 +3,6 @@ package edument.perl6idea.utils;
 public class Patterns {
     public final static String MODULE_PATTERN      = "^[A-Za-z0-9-_']++(::[A-Za-z0-9-_']++)*$";
     public final static String SCRIPT_PATTERN      = "^[A-Za-z0-9-_']++(.(p6|pl6))?$";
-    public final static String ENTRY_POINT_PATTERN = "^[A-Za-z0-9-_']++$";
+    public final static String ENTRY_POINT_PATTERN = "^[A-Za-z0-9-_'.]++$";
     public final static String TEST_PATTERN        = "^[A-Za-z0-9-_']++(.t)?$";
 }
