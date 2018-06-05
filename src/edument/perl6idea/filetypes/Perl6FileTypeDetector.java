963fca8e1695528001968307a0205255c527e093
Altai-man <altay-man@mail.ru>
2018-06-05 18:11:32 +0300

    Detect files under bin/ as Perl 6 scripts

---
+++ src/edument/perl6idea/filetypes/Perl6FileTypeDetector.java
@@ -0,0 +1,24 @@
+package edument.perl6idea.filetypes;
+
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeRegistry;
+import com.intellij.openapi.util.io.ByteSequence;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Perl6FileTypeDetector implements FileTypeRegistry.FileTypeDetector {
+    @Nullable
+    @Override
+    public FileType detect(@NotNull VirtualFile file, @NotNull ByteSequence firstBytes, @Nullable CharSequence firstCharsIfText) {
+        if (file.getExtension() != null) return null;
+        if (file.getParent() != null && file.getParent().toString().endsWith("bin"))
+            return Perl6ScriptFileType.INSTANCE;
+        return null;
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+}
