98c8812e3dbf762ff9dbe948eacd1aff344acd1e
Altai-man <altay-man@mail.ru>
2018-06-21 19:50:27 +0300

    Apply build fixes

--- perl6-idea-plugin/src/edument/perl6idea/project/Perl6ProjectStructureConfigurable.java
+++ perl6-idea-plugin/src/edument/perl6idea/project/Perl6ProjectStructureConfigurable.java
@@ -124,8 +124,8 @@ public class Perl6ProjectStructureConfigurable extends BaseConfigurable implemen
         };
 
         final DefaultActionGroup toolbarGroup = new DefaultActionGroup();
-        toolbarGroup.add(new BackAction(myComponent));
-        toolbarGroup.add(new ForwardAction(myComponent));
+        toolbarGroup.add(new BackAction(myComponent, myContext));
+        toolbarGroup.add(new ForwardAction(myComponent, myContext));
         final ActionToolbar toolbar = ActionManager.getInstance().createActionToolbar("ProjectStructure", toolbarGroup, true);
         toolbar.setTargetComponent(myComponent);
         myToolbarComponent = toolbar.getComponent();
