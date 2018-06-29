6da6f78a6d84de3de78097e06057f247dcb89869
Altai-man <altay-man@mail.ru>
2018-06-29 12:18:19 +0300

    Make trait StubBasedPsiElement

--- src/edument/perl6idea/psi/Perl6Trait.java
+++ src/edument/perl6idea/psi/Perl6Trait.java
@@ -1,6 +1,9 @@
 package edument.perl6idea.psi;
 
-public interface Perl6Trait extends Perl6PsiElement {
+import com.intellij.psi.StubBasedPsiElement;
+import edument.perl6idea.psi.stub.Perl6TraitStub;
+
+public interface Perl6Trait extends StubBasedPsiElement<Perl6TraitStub> {
     String getTraitModifier();
     String getTraitName();
 }
