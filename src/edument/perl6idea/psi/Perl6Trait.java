7ff4d2e6f45715a1d59cbe6c8ba35f27d095f43a
Altai-man <altay-man@mail.ru>
2018-07-02 15:55:39 +0300

    Trait is no more Contributor

--- src/edument/perl6idea/psi/Perl6Trait.java
+++ src/edument/perl6idea/psi/Perl6Trait.java
@@ -4,7 +4,7 @@ import com.intellij.psi.StubBasedPsiElement;
 import edument.perl6idea.psi.stub.Perl6TraitStub;
 import edument.perl6idea.psi.symbols.Perl6SymbolContributor;
 
-public interface Perl6Trait extends Perl6SymbolContributor, StubBasedPsiElement<Perl6TraitStub> {
+public interface Perl6Trait extends StubBasedPsiElement<Perl6TraitStub> {
     String getTraitModifier();
     String getTraitName();
 }
