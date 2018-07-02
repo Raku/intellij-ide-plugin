7b84e385c31340d82d733fe954b29ff169206576
Altai-man <altay-man@mail.ru>
2018-07-02 18:54:29 +0300

    Remove unused method and fix twigil

--- src/edument/perl6idea/psi/Perl6Variable.java
+++ src/edument/perl6idea/psi/Perl6Variable.java
@@ -3,20 +3,12 @@ package edument.perl6idea.psi;
 import com.intellij.psi.PsiElement;
 
 public interface Perl6Variable extends Perl6PsiElement {
-    StringBuilder stringBuilder = new StringBuilder();
 
     PsiElement getVariableToken();
 
     default String getVariableName() {
         PsiElement varToken = getVariableToken();
         return varToken != null ? varToken.getText() : null;
-    }
-
-    default String getSigilAndTwigil() {
-        stringBuilder.delete(0, stringBuilder.length());
-        stringBuilder.append(getSigil());
-        stringBuilder.append(getTwigil());
-        return stringBuilder.toString();
     }
 
     default char getSigil() {
@@ -39,6 +31,7 @@ public interface Perl6Variable extends Perl6PsiElement {
             case ':': return ':';
             case '=': return '=';
             case '~': return '~';
+            case '.': return '.';
             default:  return ' ';
         }
     }
