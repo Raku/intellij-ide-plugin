2f8c1160e1640bfb7753806ec0ac71d20531ad3d
Altai-man <altay-man@mail.ru>
2018-07-10 18:17:37 +0300

    Unused import

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6IsTraitNameImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6IsTraitNameImpl.java
@@ -2,7 +2,6 @@ package edument.perl6idea.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import edument.perl6idea.psi.Perl6IsTraitName;
 import edument.perl6idea.psi.Perl6IsTraitReference;
