7720c9bada789aa1c31691846835fafe67430364
Altai-man <altay-man@mail.ru>
2018-08-01 20:11:11 +0300

    Range quickfixes

--- perl6-idea-plugin/src/edument/perl6idea/intention/RangeIntention.java
+++ perl6-idea-plugin/src/edument/perl6idea/intention/RangeIntention.java
@@ -7,7 +7,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
-import edument.perl6idea.psi.Perl6IntLiteral;
+import edument.perl6idea.psi.*;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -33,8 +33,48 @@ public class RangeIntention extends PsiElementBaseIntentionAction implements Int
         // Start of `0..foo`
         int offset = parent.getTextOffset();
         // Offset of `foo`, last child of application
-        int offsetEnd = parent.getLastChild().getTextOffset();
-        editor.getDocument().replaceString(offset, offsetEnd, "^");
+        PsiElement last = parent.getLastChild();
+        int offsetEnd = last.getTextOffset();
+        Integer top;
+
+        // FIXME
+        if (last instanceof Perl6IntLiteral && element.getText().equals("..")) {
+            top = Integer.parseInt(last.getText());
+            editor.getDocument().replaceString(
+                offset, offsetEnd + last.getTextLength(), "^" + ++top);
+        } else if (last instanceof Perl6IntLiteral && element.getText().equals("..^")) {
+            editor.getDocument().replaceString(
+                offset, offsetEnd, "^");
+        } else if (last instanceof Perl6Variable && element.getText().equals("..^")) {
+            editor.getDocument().replaceString(
+                offset, offsetEnd, "^");
+        } else if (last instanceof Perl6InfixApplication && element.getText().equals("..")) {
+            PsiElement[] children = last.getChildren();
+            if (checkInfix(children)) {
+                editor.getDocument().replaceString(
+                    offset, offsetEnd + last.getTextLength(),
+                    "^" + children[0].getText()
+                );
+            }
+        } else if (last instanceof Perl6ParenthesizedExpr && element.getText().equals("..")) {
+            Perl6InfixApplication infixInParens = PsiTreeUtil.findChildOfType(last, Perl6InfixApplication.class);
+            if (infixInParens != null) {
+                PsiElement[] children = infixInParens.getChildren();
+                if (checkInfix(children)) {
+                    editor.getDocument().replaceString(
+                        offset, offsetEnd + last.getTextLength(),
+                        "^" + children[0].getText()
+                    );
+                }
+            }
+        }
+    }
+
+    private static boolean checkInfix(PsiElement[] children) {
+        return children.length == 3 &&
+               children[0] instanceof Perl6Variable &&
+               children[1] instanceof Perl6Infix && children[1].getText().equals("-") &&
+               children[2] instanceof Perl6IntLiteral && children[2].getText().equals("1");
     }
 
     @Override
@@ -44,7 +84,12 @@ public class RangeIntention extends PsiElementBaseIntentionAction implements Int
         if (!OPS.contains(op)) return false;
         element = element.getParent();
         PsiElement prev = PsiTreeUtil.getPrevSiblingOfType(element, Perl6IntLiteral.class);
-        return prev != null && prev.getText().equals("0");
+        PsiElement nextInt = PsiTreeUtil.getPrevSiblingOfType(element, Perl6IntLiteral.class);
+        PsiElement nextVar = PsiTreeUtil.getPrevSiblingOfType(element, Perl6Variable.class);
+        PsiElement nextInfix = PsiTreeUtil.getPrevSiblingOfType(element, Perl6InfixApplication.class);
+        PsiElement nextParens= PsiTreeUtil.getPrevSiblingOfType(element, Perl6ParenthesizedExpr.class);
+        return prev != null && prev.getText().equals("0") &&
+               (nextInt != null || nextVar != null || nextInfix != null || nextParens != null);
     }
 
     @Nls
---
+++ perl6-idea-plugin/tests/edument/perl6idea/intention/IntentionTest.java
@@ -0,0 +1,52 @@
+package edument.perl6idea.intention;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import edument.perl6idea.filetypes.Perl6ScriptFileType;
+
+public class IntentionTest extends LightCodeInsightFixtureTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return "testData/intention";
+    }
+
+    public void testZeroToN() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "for 0.<caret>.9 {}");
+        IntentionAction intention = myFixture.findSingleIntention("Remove redundant zero");
+        assertNotNull(intention);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("Range1.p6");
+    }
+
+    public void testZeroToExclusiveN() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "for 0.<caret>.^10 {}");
+        IntentionAction intention = myFixture.findSingleIntention("Remove redundant zero");
+        assertNotNull(intention);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("Range1.p6");
+    }
+
+    public void testZeroToVar() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "for 0.<caret>.^$n {}");
+        IntentionAction intention = myFixture.findSingleIntention("Remove redundant zero");
+        assertNotNull(intention);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("Range2.p6");
+    }
+
+    public void testZeroToExclusiveVar() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "for 0.<caret>.$n-1 {}");
+        IntentionAction intention = myFixture.findSingleIntention("Remove redundant zero");
+        assertNotNull(intention);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("Range2.p6");
+    }
+
+    public void testUndeclaredVariableAnnotatorReallyUndeclared() {
+        myFixture.configureByText(Perl6ScriptFileType.INSTANCE, "for 0.<caret>.($n-1) {}");
+        IntentionAction intention = myFixture.findSingleIntention("Remove redundant zero");
+        assertNotNull(intention);
+        myFixture.launchAction(intention);
+        myFixture.checkResultByFile("Range2.p6");
+    }
+}
