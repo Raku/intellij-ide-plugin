98f78ccfc3c895c7ec91e695c335c08beeae05c1
Altai-man <altay-man@mail.ru>
2018-08-02 13:48:00 +0300

    Use more precise check for intention Also make the algorithm more readable and add necessary comments

--- perl6-idea-plugin/src/edument/perl6idea/intention/RangeIntention.java
+++ perl6-idea-plugin/src/edument/perl6idea/intention/RangeIntention.java
@@ -18,7 +18,7 @@ import java.util.Set;
 import static edument.perl6idea.parsing.Perl6TokenTypes.INFIX;
 
 public class RangeIntention extends PsiElementBaseIntentionAction implements IntentionAction {
-    static final Set<String> OPS = new HashSet<>(Arrays.asList("^..", "..", "..^", "^..^"));
+    static final Set<String> OPS = new HashSet<>(Arrays.asList("..", "..^"));
 
     @NotNull
     @Override
@@ -28,45 +28,38 @@ public class RangeIntention extends PsiElementBaseIntentionAction implements Int
 
     @Override
     public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
-        // Infix application
+        // Infix application, not token and not Perl6Infix
         PsiElement parent = element.getParent().getParent();
         // Start of `0..foo`
         int offset = parent.getTextOffset();
         // Offset of `foo`, last child of application
         PsiElement last = parent.getLastChild();
         int offsetEnd = last.getTextOffset();
-        Integer top;
 
-        // FIXME
-        if (last instanceof Perl6IntLiteral && element.getText().equals("..")) {
-            top = Integer.parseInt(last.getText());
-            editor.getDocument().replaceString(
-                offset, offsetEnd + last.getTextLength(), "^" + ++top);
-        } else if (last instanceof Perl6IntLiteral && element.getText().equals("..^")) {
-            editor.getDocument().replaceString(
-                offset, offsetEnd, "^");
-        } else if (last instanceof Perl6Variable && element.getText().equals("..^")) {
-            editor.getDocument().replaceString(
-                offset, offsetEnd, "^");
-        } else if (last instanceof Perl6InfixApplication && element.getText().equals("..")) {
-            PsiElement[] children = last.getChildren();
-            if (checkInfix(children)) {
-                editor.getDocument().replaceString(
-                    offset, offsetEnd + last.getTextLength(),
-                    "^" + children[0].getText()
-                );
-            }
-        } else if (last instanceof Perl6ParenthesizedExpr && element.getText().equals("..")) {
+        String infixText = element.getText();
+        boolean isSimpleRange = infixText.equals("..");
+        if (last instanceof Perl6IntLiteral && isSimpleRange) {
+            // We can not catch exception here, as Perl6IntLiteral is always a valid number
+            int top = Integer.parseInt(last.getText());
+            editor.getDocument().replaceString(offset, offsetEnd + last.getTextLength(), "^" + ++top);
+        } else if (infixText.equals("..^") && (last instanceof Perl6IntLiteral || last instanceof Perl6Variable)) {
+            editor.getDocument().replaceString(offset, offsetEnd, "^");
+        } else if (last instanceof Perl6InfixApplication && isSimpleRange) {
+            handleInfix(editor, offset, last, offsetEnd, last.getTextLength());
+        } else if (last instanceof Perl6ParenthesizedExpr && isSimpleRange) {
             Perl6InfixApplication infixInParens = PsiTreeUtil.findChildOfType(last, Perl6InfixApplication.class);
-            if (infixInParens != null) {
-                PsiElement[] children = infixInParens.getChildren();
-                if (checkInfix(children)) {
-                    editor.getDocument().replaceString(
-                        offset, offsetEnd + last.getTextLength(),
-                        "^" + children[0].getText()
-                    );
-                }
-            }
+            if (infixInParens != null)
+                handleInfix(editor, offset, infixInParens, offsetEnd, last.getTextLength());
+        }
+    }
+
+    private static void handleInfix(Editor editor, int offset, PsiElement last, int offsetEnd, int length) {
+        PsiElement[] children = last.getChildren();
+        if (checkInfix(children)) {
+            editor.getDocument().replaceString(
+                offset, offsetEnd + length,
+                "^" + children[0].getText()
+            );
         }
     }
 
@@ -82,14 +75,34 @@ public class RangeIntention extends PsiElementBaseIntentionAction implements Int
         if (!(element.getNode().getElementType() == INFIX)) return false;
         String op = element.getText();
         if (!OPS.contains(op)) return false;
+        // Not PsiElement with token, but Perl6Infix
         element = element.getParent();
+        // Get and check possible siblings
+        PsiElement temp = element.getNextSibling();
+        while (temp != null) {
+            if (temp instanceof Perl6IntLiteral || temp instanceof Perl6Variable ||
+                temp instanceof Perl6InfixApplication || temp instanceof Perl6ParenthesizedExpr)
+                break;
+            temp = temp.getNextSibling();
+        }
+
         PsiElement prev = PsiTreeUtil.getPrevSiblingOfType(element, Perl6IntLiteral.class);
-        PsiElement nextInt = PsiTreeUtil.getPrevSiblingOfType(element, Perl6IntLiteral.class);
-        PsiElement nextVar = PsiTreeUtil.getPrevSiblingOfType(element, Perl6Variable.class);
-        PsiElement nextInfix = PsiTreeUtil.getPrevSiblingOfType(element, Perl6InfixApplication.class);
-        PsiElement nextParens= PsiTreeUtil.getPrevSiblingOfType(element, Perl6ParenthesizedExpr.class);
-        return prev != null && prev.getText().equals("0") &&
-               (nextInt != null || nextVar != null || nextInfix != null || nextParens != null);
+        Perl6InfixApplication infixInParens = null;
+        if (temp instanceof Perl6ParenthesizedExpr)
+            infixInParens = PsiTreeUtil.findChildOfType(temp, Perl6InfixApplication.class);
+        return prev != null && prev.getText().equals("0") && // Basic condition
+               // Int condition
+               (temp instanceof Perl6IntLiteral && (element.getText().equals("..") || element.getText().equals("..^")) ||
+                // Var condition
+                temp instanceof Perl6Variable && element.getText().equals("..^") ||
+                // Infix, possible `0..$n-1`
+                temp instanceof Perl6InfixApplication && element.getText().equals("..") ||
+                // Parens, possible ..($n-1)
+                temp instanceof Perl6ParenthesizedExpr && element.getText().equals("..") &&
+                ( // inner parens expr
+                  infixInParens != null && checkInfix(infixInParens.getChildren())
+                )
+               );
     }
 
     @Nls
