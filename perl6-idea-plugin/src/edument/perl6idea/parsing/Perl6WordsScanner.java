16bc2c8c5546f87516eb46a7d99ec7a778d117e5
Altai-man <altay-man@mail.ru>
2018-08-16 19:07:13 +0300

    Use proper token sets

--- perl6-idea-plugin/src/edument/perl6idea/parsing/Perl6WordsScanner.java
+++ perl6-idea-plugin/src/edument/perl6idea/parsing/Perl6WordsScanner.java
@@ -5,7 +5,16 @@ import com.intellij.psi.tree.TokenSet;
 
 public class Perl6WordsScanner extends DefaultWordsScanner {
     public Perl6WordsScanner() {
-        super(new Perl6Lexer(), TokenSet.create(Perl6TokenTypes.NAME),
-              TokenSet.EMPTY, TokenSet.create(Perl6TokenTypes.INTEGER_LITERAL));
+        super(new Perl6Lexer(),
+              // Identifiers
+              TokenSet.create(Perl6TokenTypes.NAME,
+                              Perl6TokenTypes.SUB_CALL_NAME,
+                              Perl6TokenTypes.METHOD_CALL_NAME),
+              // Comments
+              TokenSet.create(Perl6TokenTypes.COMMENT),
+              // Literals, no string literal here
+              TokenSet.create(
+                  Perl6TokenTypes.INTEGER_LITERAL, Perl6TokenTypes.COMPLEX_LITERAL,
+                  Perl6TokenTypes.NUMBER_LITERAL, Perl6TokenTypes.RAT_LITERAL));
     }
 }
