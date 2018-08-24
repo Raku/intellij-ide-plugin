87ddf936e87c6acc4686bdb9db60ca67bb89160f
Altai-man <altay-man@mail.ru>
2018-08-24 16:24:35 +0300

    Add `getTypeName` for Perl6Trusts

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6TrustsImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6TrustsImpl.java
@@ -2,11 +2,18 @@ package edument.perl6idea.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import edument.perl6idea.psi.Perl6Trusts;
+import edument.perl6idea.psi.Perl6TypeName;
 
 public class Perl6TrustsImpl extends ASTWrapperPsiElement implements Perl6Trusts {
     public Perl6TrustsImpl(ASTNode node) {
         super(node);
+    }
+
+    @Override
+    public String getTypeName() {
+        Perl6TypeName name = PsiTreeUtil.findChildOfType(this, Perl6TypeName.class);
+        return name == null ? "" : name.getTypeName();
     }
 }
