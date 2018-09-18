fef20c23c1ad273dde742d08d44dac680a3ea04f
Altai-man <altay-man@mail.ru>
2018-09-18 17:15:23 +0300

    Use custom spell checking strategy From now on only check text under COMMENT token.

---
+++ perl6-idea-plugin/src/edument/perl6idea/editor/Perl6SpellCheckingStrategy.java
@@ -0,0 +1,31 @@
+package edument.perl6idea.editor;
+
+import com.intellij.codeInspection.SuppressionUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.spellchecker.inspections.CommentSplitter;
+import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
+import com.intellij.spellchecker.tokenizer.TokenConsumer;
+import com.intellij.spellchecker.tokenizer.Tokenizer;
+import edument.perl6idea.parsing.Perl6TokenTypes;
+import org.jetbrains.annotations.NotNull;
+
+public class Perl6SpellCheckingStrategy extends SpellcheckingStrategy {
+    private Tokenizer perl6CommentTokenizer = new Tokenizer() {
+        @Override
+        public void tokenize(@NotNull PsiElement element, TokenConsumer consumer) {
+            consumer.consumeToken(element, CommentSplitter.getInstance());
+        }
+    };
+
+    @NotNull
+    @Override
+    public Tokenizer getTokenizer(PsiElement element) {
+        if ( element.getNode().getElementType() == Perl6TokenTypes.COMMENT) {
+            if (SuppressionUtil.isSuppressionComment(element)) {
+                return EMPTY_TOKENIZER;
+            }
+            return perl6CommentTokenizer;
+        }
+        return EMPTY_TOKENIZER;
+    }
+}
