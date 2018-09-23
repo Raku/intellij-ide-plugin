1a078977eaa5b91cf106fdbdc746c9a309085464
Altai-man <altay-man@mail.ru>
2018-09-23 15:18:43 +0300

    Add simplest InputValidator to be used with rename It does not enforces any rules right now, but lifts up java restriction set by default by Idea.

---
+++ perl6-idea-plugin/src/edument/perl6idea/rename/Perl6InputValidator.java
@@ -0,0 +1,17 @@
+package edument.perl6idea.rename;
+
+import com.intellij.lang.refactoring.NamesValidator;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class Perl6InputValidator implements NamesValidator {
+    @Override
+    public boolean isKeyword(@NotNull String name, Project project) {
+        return false;
+    }
+
+    @Override
+    public boolean isIdentifier(@NotNull String name, Project project) {
+        return true;
+    }
+}
