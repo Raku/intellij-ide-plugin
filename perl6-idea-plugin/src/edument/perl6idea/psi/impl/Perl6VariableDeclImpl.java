ff0bf2622e0b9c5d49462ef1754d2a55ada8be93
Altai-man <altay-man@mail.ru>
2018-09-27 15:14:52 +0300

    Remove useless comment Somehow renaming on variables works without this implementation.

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6VariableDeclImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6VariableDeclImpl.java
@@ -47,8 +47,7 @@ public class Perl6VariableDeclImpl extends Perl6MemberStubBasedPsi<Perl6Variable
 
     @Override
     public PsiElement setName(@NotNull String s) throws IncorrectOperationException {
-        // TODO See https://github.com/JetBrains/intellij-community/blob/db9200fcdb58eccfeb065524bd211b3aa6d6b83c/java/java-psi-impl/src/com/intellij/psi/impl/PsiImplUtil.java
-        return null;
+        return this;
     }
 
     @Override
