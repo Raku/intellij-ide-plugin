98d3d4dca49563681adcda45f0fc73b394d24200
Altai-man <altay-man@mail.ru>
2018-10-18 22:14:39 +0300

    Apply validator to variable form

--- perl6-idea-plugin/src/edument/perl6idea/refactoring/introduce/Perl6IntroduceDialog.java
+++ perl6-idea-plugin/src/edument/perl6idea/refactoring/introduce/Perl6IntroduceDialog.java
@@ -1,21 +1,27 @@
 package edument.perl6idea.refactoring.introduce;
 
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.EditorComboBoxEditor;
 import com.intellij.ui.EditorComboBoxRenderer;
+import com.intellij.ui.EditorTextField;
 import com.intellij.ui.StringComboboxEditor;
 import edument.perl6idea.filetypes.Perl6ScriptFileType;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Collection;
 
 public class Perl6IntroduceDialog extends DialogWrapper {
+    private final String myHelpId;
+    private final IntroduceValidator myValidator;
     private ComboBox myNameComboBox;
     private Project myProject;
-    private JButton myCreateButton;
     private JPanel myContentPane;
 
     public Perl6IntroduceDialog(Project project,
@@ -25,10 +31,19 @@ public class Perl6IntroduceDialog extends DialogWrapper {
                                 IntroduceOperation operation) {
         super(project, true);
         myProject = project;
+        myHelpId = id;
+        myValidator = validator;
 
         setTitle(title);
         setUpNameComboBox(operation.getSuggestedNames());
         init();
+        updateControls();
+    }
+
+    @Nullable
+    @Override
+    protected String getHelpId() {
+        return myHelpId;
     }
 
     private void setUpNameComboBox(Collection<String> names) {
@@ -37,10 +52,29 @@ public class Perl6IntroduceDialog extends DialogWrapper {
         myNameComboBox.setRenderer(new EditorComboBoxRenderer(comboBoxEditor));
         myNameComboBox.setEditable(true);
         myNameComboBox.setMaximumRowCount(8);
+        myNameComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                updateControls();
+            }
+        });
+        ((EditorTextField)myNameComboBox.getEditor().getEditorComponent()).addDocumentListener(new DocumentListener() {
+            public void documentChanged(DocumentEvent event) {
+                updateControls();
+            }
+        });
+
+        myNameComboBox.requestFocusInWindow();
 
         for (String possibleName : names) {
             myNameComboBox.addItem(possibleName);
         }
+    }
+
+    private void updateControls() {
+        boolean nameValid = myValidator.isNameValid(getName());
+        setOKActionEnabled(nameValid);
+        setErrorText(!nameValid ? "Name is invalid" : null, myNameComboBox);
     }
 
     @Nullable
