d55a79340fde7d14cff06e2c23cbf9297eb2ffbf
Altai-man <altay-man@mail.ru>
2018-10-18 22:13:27 +0300

    Add basic set of rules for IntroduceValidator

--- perl6-idea-plugin/src/edument/perl6idea/refactoring/introduce/IntroduceValidator.java
+++ perl6-idea-plugin/src/edument/perl6idea/refactoring/introduce/IntroduceValidator.java
@@ -1,4 +1,13 @@
 package edument.perl6idea.refactoring.introduce;
 
 public class IntroduceValidator {
+    public static boolean isNameValid(String name) {
+        return name != null && isIdentifier(name);
+    }
+
+    private static boolean isIdentifier(String name) {
+        return !name.endsWith("-") && !name.startsWith("-") && !(Character.isDigit(name.charAt(0))) &&
+               name.startsWith("$") || name.startsWith("@") || name.startsWith("%") || name.startsWith("&") ||
+               name.startsWith("\\");
+    }
 }
