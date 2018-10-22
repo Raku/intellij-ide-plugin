d6c07cb55c4a6d169b23e73b6850bcfe17e1cb43
Altai-man <altay-man@mail.ru>
2018-10-22 15:52:22 +0300

    Basic inference for method call return type

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6PostfixApplicationImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6PostfixApplicationImpl.java
@@ -3,8 +3,10 @@ package edument.perl6idea.psi.impl;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
 import edument.perl6idea.psi.Perl6MethodCall;
 import edument.perl6idea.psi.Perl6PostfixApplication;
+import edument.perl6idea.psi.Perl6RoutineDecl;
 import edument.perl6idea.psi.Perl6TypeName;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,9 +18,24 @@ public class Perl6PostfixApplicationImpl extends ASTWrapperPsiElement implements
     @Override
     public String inferType() {
         PsiElement first = getFirstChild();
-        if (!(first instanceof Perl6TypeName)) return "Any";
         PsiElement last = getLastChild();
-        if (!(last instanceof Perl6MethodCall)) return "Any";
-        return ((Perl6MethodCall)last).getCallName().equals(".new") ? ((Perl6TypeName)first).getTypeName() : "Any";
+
+        if (first instanceof Perl6TypeName && last instanceof Perl6MethodCall) {
+            Perl6MethodCall call = (Perl6MethodCall)last;
+            Perl6TypeName typeName = (Perl6TypeName)first;
+            return call.getCallName().equals(".new") ? typeName.getTypeName() : tryToCalculateMethodReturnType(call);
+        } else if (last instanceof Perl6MethodCall) {
+            return tryToCalculateMethodReturnType((Perl6MethodCall)last);
+        }
+        return "Any";
+    }
+
+    private static String tryToCalculateMethodReturnType(Perl6MethodCall last) {
+        PsiReference ref = last.getReference();
+        if (ref == null) return "Any";
+        PsiElement resolved = ref.resolve();
+        if (resolved == null) return "Any";
+        Perl6RoutineDecl decl = (Perl6RoutineDecl)resolved;
+        return decl.getReturnType();
     }
 }
