7804f17fa577a25d54018bc5728e4c492c69698f
Altai-man <altay-man@mail.ru>
2018-11-12 17:41:56 +0200

    Updates tests to match new API

--- perl6-idea-plugin/tests/edument/perl6idea/actions/ModuleCompletionTest.java
+++ perl6-idea-plugin/tests/edument/perl6idea/actions/ModuleCompletionTest.java
@@ -6,41 +6,51 @@ import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.PsiManagerImpl;
 import com.intellij.psi.impl.file.PsiDirectoryImpl;
+import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import edument.perl6idea.Perl6LightProjectDescriptor;
+import edument.perl6idea.module.Perl6MetaDataComponent;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 
 public class ModuleCompletionTest extends LightPlatformCodeInsightFixtureTestCase {
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new Perl6LightProjectDescriptor();
+    }
+
     public void testNewModuleProperties() {
         PsiManagerImpl psiManager = (PsiManagerImpl)PsiManager.getInstance(getProject());
         VirtualFile baseDir = getProject().getBaseDir();
         NewModuleAction newModuleAction = new NewModuleAction();
-        newModuleAction.setBaseDir(baseDir.getCanonicalPath());
 
         // Test project dir case
         PsiDirectoryImpl psiDirectory = new PsiDirectoryImpl(psiManager, baseDir);
-        assertNull(newModuleAction.processNavigatable(psiDirectory));
-        assertEquals(newModuleAction.getBaseDir(), baseDir.getCanonicalPath());
+        assertEmpty(newModuleAction.processNavigatable(myModule, psiDirectory));
+        assertEquals(newModuleAction.getBaseDir(), baseDir.getPath());
 
         // Test 'lib' case
-        newModuleAction.setBaseDir(Paths.get(baseDir.getCanonicalPath(), "lib").toString());
+        newModuleAction.setBaseDir(Paths.get(baseDir.getPath(), "lib").toString());
         VirtualFile lib = getOrCreateDirectory(baseDir, "lib");
         psiDirectory = new PsiDirectoryImpl(psiManager, lib);
-        assertNull(newModuleAction.processNavigatable(psiDirectory));
-        assertEquals(newModuleAction.getBaseDir(), lib.getCanonicalPath());
+        assertEmpty(newModuleAction.processNavigatable(myModule, psiDirectory));
+        assertEquals(newModuleAction.getBaseDir(), lib.getPath());
 
         // Test 'lib/Foo' case
+        newModuleAction.setBaseDir(lib.getPath());
         VirtualFile foo = getOrCreateDirectory(lib, "Foo");
         PsiDirectory insideLib = new PsiDirectoryImpl(psiManager, foo);
-        assertEquals("Foo::", newModuleAction.processNavigatable(insideLib));
-        assertEquals(newModuleAction.getBaseDir(), Paths.get(baseDir.getCanonicalPath(), "lib").toString());
+        assertEquals("Foo::", newModuleAction.processNavigatable(myModule, insideLib));
+        assertEquals(newModuleAction.getBaseDir(), lib.getPath());
 
         // Test 't' case
+        String baseDir1 = Paths.get(baseDir.getPath(), "t").toString();
+        newModuleAction.setBaseDir(baseDir1);
         VirtualFile t = getOrCreateDirectory(baseDir, "t");
         PsiDirectory insideTests = new PsiDirectoryImpl(psiManager, t);
-        assertEmpty(newModuleAction.processNavigatable(insideTests));
-        assertTrue(newModuleAction.getBaseDir().equals(Paths.get(baseDir.getCanonicalPath(), "t").toString()));
+        assertEmpty(newModuleAction.processNavigatable(myModule, insideTests));
+        assertTrue(newModuleAction.getBaseDir().equals(baseDir1));
         this.removeLeftovers(lib, foo, t);
     }
 
@@ -53,7 +63,7 @@ public class ModuleCompletionTest extends LightPlatformCodeInsightFixtureTestCas
                 });
     }
 
-    VirtualFile getOrCreateDirectory(VirtualFile base, String name) {
+    private VirtualFile getOrCreateDirectory(VirtualFile base, String name) {
         if (base.findFileByRelativePath(name) == null)
             ApplicationManager.getApplication().runWriteAction(() -> {
                 try {
--- perl6-idea-plugin/tests/edument/perl6idea/actions/NewActionsTest.java
+++ perl6-idea-plugin/tests/edument/perl6idea/actions/NewActionsTest.java
@@ -2,35 +2,58 @@ package edument.perl6idea.actions;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.ex.temp.TempFileSystem;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl;
+import edument.perl6idea.Perl6LightProjectDescriptor;
+import edument.perl6idea.module.Perl6MetaDataComponent;
 import edument.perl6idea.module.Perl6ModuleBuilder;
+import org.jetbrains.annotations.SystemIndependent;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class NewActionsTest extends LightPlatformCodeInsightFixtureTestCase {
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new Perl6LightProjectDescriptor();
+    }
+
     public void testNewScriptAction() {
-        Perl6ModuleBuilder.stubScript(getProject().getBasePath(), "test.p6");
-        File path = Paths.get(getProject().getBasePath(), "test.p6").toFile();
+        @SystemIndependent String basePath = getProject().getBasePath();
+        Perl6ModuleBuilder.stubScript(basePath, "test.p6");
+        File path = Paths.get(basePath, "test.p6").toFile();
         assertTrue(path.exists());
     }
 
-    public void testNewModuleAction() {
-        Project p = getProject();
-        String basePath = p.getBasePath();
-        Perl6ModuleBuilder.stubModule(p, basePath, "Foo::Bar", null, true);
+    public void testNewModuleAction() throws IOException {
+        Perl6MetaDataComponent metaData = myModule.getComponent(Perl6MetaDataComponent.class);
+        String basePath = Paths.get(getProject().getBasePath(), "lib").toString();
+        VirtualFile libVirtualFile = VirtualFileManager.getInstance().findFileByUrl("temp:///lib");
+        Perl6ModuleBuilder.stubModule(metaData, basePath, "Foo::Bar", true, false, libVirtualFile);
         assertExists(Paths.get(basePath, "Foo", "Bar.pm6").toFile());
-        Perl6ModuleBuilder.stubModule(p, Paths.get(basePath, "lib").toString(), "Foo::Bar", null, true);
+
+        VirtualFile meta = libVirtualFile.getParent().findChild("META6.json");
+        checkMETA(meta,"Foo::Bar", "Foo/Bar.pm6");
+        Perl6ModuleBuilder.stubModule(metaData, Paths.get(basePath, "lib").toString(), "Foo::Bar", false, false, null);
         assertExists(Paths.get(basePath, "lib", "Foo", "Bar.pm6").toFile());
-        assertExists(Paths.get(basePath, "META6.json").toFile());
-        checkMETA(basePath,"Foo::Bar", "Foo/Bar.pm6");
-        Perl6ModuleBuilder.stubModule(p, Paths.get(basePath, "lib").toString(), "Foo::Baz", null, false);
+        assertTrue(meta.exists());
+
+        Perl6ModuleBuilder.stubModule(metaData, Paths.get(basePath, "lib").toString(), "Foo::Baz", false, false,null);
         assertExists(Paths.get(basePath, "lib", "Foo", "Baz.pm6").toFile());
-        checkMETA(basePath,"Foo::Bar", "Foo/Bar.pm6");
-        checkMETA(basePath, "Foo::Baz", "Foo/Baz.pm6");
+        checkMETA(meta,"Foo::Bar", "Foo/Bar.pm6");
+        checkMETA(meta, "Foo::Baz", "Foo/Baz.pm6");
     }
 
     public void testNewTestAction() {
@@ -42,12 +65,11 @@ public class NewActionsTest extends LightPlatformCodeInsightFixtureTestCase {
         assertExists(Paths.get(basePath, "t", "20-sanity.t").toFile());
     }
 
-    private static void checkMETA(String basePath, String name, String path) {
+    private static void checkMETA(VirtualFile basePath, String name, String path) {
         try {
-            String metaInfo =
-              new String(Files.readAllBytes(Paths.get(basePath, "META6.json")), CharsetToolkit.UTF8_CHARSET);
-            assertTrue(metaInfo.contains(name));
-            assertTrue(metaInfo.contains(path));
+            String metaContent = new String(basePath.contentsToByteArray(), CharsetToolkit.UTF8_CHARSET);
+            assertTrue(metaContent.contains(name));
+            assertTrue(metaContent.contains(path));
         } catch (IOException e) {
             assertTrue(false);
         }
