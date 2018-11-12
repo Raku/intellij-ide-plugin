f691f104c0abc9b063fe0808b16f6657c2da2525
Altai-man <altay-man@mail.ru>
2018-11-12 11:57:47 +0200

    Use new metadata API for file changes listener

--- perl6-idea-plugin/src/edument/perl6idea/event/ModuleMetaChangeListener.java
+++ perl6-idea-plugin/src/edument/perl6idea/event/ModuleMetaChangeListener.java
@@ -1,9 +1,8 @@
 package edument.perl6idea.event;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleComponent;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
@@ -13,110 +12,95 @@ import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFileMoveEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFilePropertyChangeEvent;
 import com.intellij.util.messages.MessageBusConnection;
+import edument.perl6idea.filetypes.Perl6ModuleFileType;
+import edument.perl6idea.module.Perl6MetaDataComponent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.json.JSONObject;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ModuleMetaChangeListener implements ApplicationComponent, BulkFileListener {
+public class ModuleMetaChangeListener implements ModuleComponent, BulkFileListener {
     private final MessageBusConnection conn;
-    private static Logger LOGGER = Logger.getInstance(ModuleMetaChangeListener.class);
+    private final Perl6MetaDataComponent myMetaData;
+
+    public ModuleMetaChangeListener(Module module) {
+        conn = ApplicationManager.getApplication().getMessageBus().connect();
+        myMetaData = module.getComponent(Perl6MetaDataComponent.class);
+    }
 
     @Nullable
-    private static List<String> calculateModuleName(String path) {
-        Matcher m = Pattern.compile("(.*)/lib/(.+).pm6").matcher(path);
+    private static String calculateModuleName(String path) {
+        Matcher m = Pattern.compile(".*?/lib/(.+).pm6").matcher(path);
         if (m.matches()) {
-            return Arrays.asList(m.group(1),
-                                 m.group(2).replaceAll("/", "::"));
+            return m.group(1)
+                    .replaceAll("/", "::")
+                    .replaceAll("\\\\", "::");
         }
         return null;
     }
 
     @Override
-    public void before(@NotNull List<? extends VFileEvent> list) {
-    }
+    public void before(@NotNull List<? extends VFileEvent> list) {}
 
     @Override
     public void after(@NotNull List<? extends VFileEvent> events) {
         for (VFileEvent event : events) {
-            if (event instanceof VFileDeleteEvent) {
-                VFileDeleteEvent evt = (VFileDeleteEvent)event;
-                processEvent(evt);
-            } else if (event instanceof VFilePropertyChangeEvent) {
-                VFilePropertyChangeEvent evt = (VFilePropertyChangeEvent)event;
-                if (Objects.equals(evt.getPropertyName(), VirtualFile.PROP_NAME)) {
-                    processEvent(evt);
-                }
-            } else if (event instanceof VFileMoveEvent) {
-                VFileMoveEvent evt = (VFileMoveEvent)event;
-                processEvent(evt);
-            }
-        }
-    }
+            VirtualFile file = event.getFile();
 
-    private static void processEvent(VFileDeleteEvent evt) {
-        if (Objects.equals(evt.getFile().getExtension(), "pm6")) {
-            List<String> metaAndModule = calculateModuleName(evt.getPath());
-            if (metaAndModule == null) return;
-            Path metaPath = Paths.get(metaAndModule.get(0), "META6.json");
-            String oldName = metaAndModule.get(1);
-            updateMeta(metaPath, oldName, null);
+            if (file == null || // File might be null
+                // It is not a directory and its path does not end with `.pm6`, so skip it
+                !file.isDirectory() && !(Objects.equals(file.getExtension(), Perl6ModuleFileType.INSTANCE.getDefaultExtension())))
+                continue;
+
+            // TODO Handle directories, not only files
+
+            if (event instanceof VFileDeleteEvent) {
+                if (file.isDirectory())
+                    System.out.println("Directory is deleted");
+                else
+                    processFileDelete(event);
+            }
+            else if (event instanceof VFilePropertyChangeEvent &&
+                     Objects.equals(((VFilePropertyChangeEvent)event).getPropertyName(), VirtualFile.PROP_NAME) ||
+                     event instanceof VFileMoveEvent) {
+                if (file.isDirectory())
+                    System.out.println("Directory is moved or renamed");
+                else
+                    processFileMoveAndRename(event);
+            } else {
+                // If it another type of event, we don't want to update LocalFileSystem
+                continue;
+            }
+
             LocalFileSystem.getInstance().refresh(false);
         }
     }
 
-    private static void processEvent(VFilePropertyChangeEvent evt) {
-        if (Objects.equals(evt.getFile().getExtension(), "pm6")) {
-            List<String> metaAndModule = calculateModuleName(evt.getOldPath());
-            if (metaAndModule == null) return;
-            Path metaPath = Paths.get(metaAndModule.get(0), "META6.json");
-            String oldName = metaAndModule.get(1);
-            String newName = calculateModuleName(evt.getPath()).get(1);
-            updateMeta(metaPath, oldName, newName);
-        }
+    private void processFileMoveAndRename(VFileEvent event) {
+        String oldPathRaw = event instanceof VFilePropertyChangeEvent ?
+                            ((VFilePropertyChangeEvent)event).getOldPath() :
+                            ((VFileMoveEvent)event).getOldPath();
+        String oldName = calculateModuleName(oldPathRaw);
+        String newName = calculateModuleName(event.getPath());
+        if (oldName != null && newName != null)
+            updateMeta(oldName, newName);
     }
 
-    private static void processEvent(VFileMoveEvent evt) {
-        if (Objects.equals(evt.getFile().getExtension(), "pm6")) {
-            List<String> metaAndModule = calculateModuleName(evt.getOldPath());
-            if (metaAndModule == null) return;
-            Path metaPath = Paths.get(metaAndModule.get(0), "META6.json");
-            String oldName = metaAndModule.get(1);
-            String newName = calculateModuleName(evt.getPath()).get(1);
-            updateMeta(metaPath, oldName, newName);
-        }
+    private void processFileDelete(VFileEvent event) {
+        String oldName = calculateModuleName(event.getPath());
+        if (oldName != null)
+            updateMeta(oldName, null);
     }
 
-    private static void updateMeta(Path metaPath, String oldName, String newName) {
-        try {
-            String content = new String(Files.readAllBytes(metaPath), CharsetToolkit.UTF8_CHARSET);
-            JSONObject metaInfo = new JSONObject(content);
-            JSONObject providesSection = metaInfo.getJSONObject("provides");
-            providesSection.remove(oldName);
-            if (newName != null) {
-                providesSection.put(newName,
-                        String.format("lib/%s.pm6", newName.replaceAll("::", "/")));
-            }
-            metaInfo.put("provides", providesSection);
-            Files.write(metaPath, Collections.singletonList(metaInfo.toString(4)), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            LOGGER.warn(e);
+    private void updateMeta(String oldName, String newName) {
+        myMetaData.removeNamespaceToProvides(oldName);
+        if (newName != null) {
+            String libBasedModulePath = String.format("lib/%s.pm6", newName.replaceAll("::", "/"));
+            myMetaData.addNamespaceToProvides(newName, libBasedModulePath);
         }
-    }
-
-    public ModuleMetaChangeListener() {
-        conn = ApplicationManager.getApplication().getMessageBus().connect();
     }
 
     @Override
