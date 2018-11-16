c78c13ce8049bc104c8b75827b4595620c05f18e
Altai-man <altay-man@mail.ru>
2018-11-16 23:22:14 +0200

    Fix thinko

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6RoutineDeclImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6RoutineDeclImpl.java
@@ -122,7 +122,7 @@ public class Perl6RoutineDeclImpl extends Perl6MemberStubBasedPsi<Perl6RoutineDe
     @Override
     public PsiElement setName(@NotNull String name) throws IncorrectOperationException {
         if (!isPrivate() && getRoutineKind().equals("method"))
-            throw new IncorrectOperationException("Rename for non-private messages is not yet supported");
+            throw new IncorrectOperationException("Rename for non-private methods is not yet supported");
         Perl6LongName longName =
             Perl6ElementFactory.createPrivateMethodCall(getProject(), name);
         ASTNode keyNode = findChildByClass(Perl6LongName.class).getNode();
