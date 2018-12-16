255a3ad8242be7fff57aab9c5ad21aab8ff1091f
Altai-man <altay-man@mail.ru>
2018-12-16 17:29:41 +0200

    Simplify internals of Perl6ElementFactory

--- perl6-idea-plugin/src/edument/perl6idea/psi/Perl6ElementFactory.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/Perl6ElementFactory.java
@@ -13,12 +13,10 @@ import java.util.List;
 import java.util.StringJoiner;
 
 public class Perl6ElementFactory {
-    public static PsiElement createNamedCodeBlock(Project project,
+    public static Perl6Statement createNamedCodeBlock(Project project,
                                                   Perl6ExtractCodeBlockHandler.NewCodeBlockData data,
                                                   List<String> contents) {
-        String text = getNamedCodeBlockText(data, contents);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6Statement.class);
+        return produceElement(project, getNamedCodeBlockText(data, contents), Perl6Statement.class);
     }
 
     private static String getNamedCodeBlockText(Perl6ExtractCodeBlockHandler.NewCodeBlockData data,
@@ -40,23 +38,19 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6Statement createStatementFromText(Project project, String def) {
-        return PsiTreeUtil.findChildOfType(createFile(project, def), Perl6Statement.class);
+        return produceElement(project, def, Perl6Statement.class);
     }
 
-    public static PsiElement createConstantAssignment(Project project, String name, String code) {
-        String text = getConstantAssignmentText(name, code);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6Statement.class);
+    public static Perl6Statement createConstantAssignment(Project project, String name, String code) {
+        return produceElement(project, getConstantAssignmentText(name, code), Perl6Statement.class);
     }
 
     private static String getConstantAssignmentText(String name, String code) {
         return String.format("my constant %s = %s;", name, code);
     }
 
-    public static PsiElement createVariableAssignment(Project project, String name, String code, boolean control) {
-        String text = getVariableAssignmentText(name, code, control);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6Statement.class);
+    public static Perl6Statement createVariableAssignment(Project project, String name, String code, boolean control) {
+        return produceElement(project, getVariableAssignmentText(name, code, control), Perl6Statement.class);
     }
 
     private static String getVariableAssignmentText(String name, String code, boolean control) {
@@ -64,9 +58,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6LongName createModuleName(Project project, String name) {
-        String text = getModuleNameText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6LongName.class);
+        return produceElement(project, getModuleNameText(name), Perl6LongName.class);
     }
 
     private static String getModuleNameText(String name) {
@@ -74,21 +66,16 @@ public class Perl6ElementFactory {
     }
 
     public static PsiElement createTypeDeclarationName(Project project, String name) {
-        String text = getTypeDeclarationText(name);
-        Perl6File dummyFile = createFile(project, text);
-        // first 6 characters are `class ` in `getTypeDeclarationText` dummy
-        return dummyFile.findElementAt(6);
+        // 6 number of chars in `class `
+        return produceElement(project, getTypeDeclarationText(name), Perl6Statement.class).findElementAt(6);
     }
 
     private static String getTypeDeclarationText(String name) {
-        // It gives us 6 characters to skip when taking exact node for `createTypeDeclarationName` method
         return String.format("class %s {}", name);
     }
 
     public static Perl6LongName createIsTraitName(Project project, String name) {
-        String text = getIsTraitNameText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6LongName.class);
+        return produceElement(project, getIsTraitNameText(name), Perl6LongName.class);
     }
 
     private static String getIsTraitNameText(String name) {
@@ -96,9 +83,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6LongName createTypeName(Project project, String name) {
-        String text = getTypeNameText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6LongName.class);
+        return produceElement(project, getTypeNameText(name), Perl6LongName.class);
     }
 
     private static String getTypeNameText(String name) {
@@ -106,9 +91,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6SubCallName createSubCallName(Project project, String name) {
-        String text = getSubroutineText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6SubCallName.class);
+        return produceElement(project, getSubroutineText(name), Perl6SubCallName.class);
     }
 
     private static String getSubroutineText(String name) {
@@ -116,9 +99,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6LongName createPublicMethodCall(Project project, String name) {
-        String text = getPublicMethodCallText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6LongName.class);
+        return produceElement(project, getPublicMethodCallText(name), Perl6LongName.class);
     }
 
     private static String getPublicMethodCallText(String name) {
@@ -126,9 +107,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6LongName createPrivateMethodCall(Project project, String name) {
-        String text = getPrivateMethodCallText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6LongName.class);
+        return produceElement(project, getPrivateMethodCallText(name), Perl6LongName.class);
     }
 
     private static String getPrivateMethodCallText(String name) {
@@ -136,9 +115,7 @@ public class Perl6ElementFactory {
     }
 
     public static Perl6Variable createVariable(Project project, String name) {
-        String text = getVariableText(name);
-        Perl6File dummyFile = createFile(project, text);
-        return PsiTreeUtil.findChildOfType(dummyFile, Perl6Variable.class);
+        return produceElement(project, getVariableText(name), Perl6Variable.class);
     }
 
     private static String getVariableText(String name) {
@@ -148,9 +125,10 @@ public class Perl6ElementFactory {
         return name;
     }
 
-    private static Perl6File createFile(Project project, String text) {
+    private static <T extends PsiElement> T produceElement(Project project, String text, Class<T> clazz) {
         String filename = "dummy." + Perl6ScriptFileType.INSTANCE.getDefaultExtension();
-        return (Perl6File)PsiFileFactory.getInstance(project)
-                                        .createFileFromText(filename, Perl6ScriptFileType.INSTANCE, text);
+        Perl6File dummyFile = (Perl6File) PsiFileFactory.getInstance(project)
+                .createFileFromText(filename, Perl6ScriptFileType.INSTANCE, text);
+        return PsiTreeUtil.findChildOfType(dummyFile, clazz);
     }
 }
