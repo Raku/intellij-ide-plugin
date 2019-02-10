5ca794dd62850f8b06b2d11f981aff95a2e0f2f4
Altai-man <altay-man@mail.ru>
2019-02-10 18:20:01 +0200

    Fix indent

--- perl6-idea-plugin/src/edument/perl6idea/run/Perl6ProfileExecutor.java
+++ perl6-idea-plugin/src/edument/perl6idea/run/Perl6ProfileExecutor.java
@@ -10,45 +10,45 @@ public class Perl6ProfileExecutor extends DefaultRunExecutor {
     public static final String RUN_WITH_PERL_6_PROFILER = "Profile";
     public static final String RUN_WITH_PERL_6_PROFILER1 = "RunWithPerl6Profiler";
 
-	@NotNull
-	public String getToolWindowId() {
+    @NotNull
+    public String getToolWindowId() {
             return getId();
 	}
 
-	@NotNull
-	public Icon getIcon() {
+    @NotNull
+    public Icon getIcon() {
             return Perl6Icons.CAMELIA;
 	}
 
-	public Icon getDisabledIcon() {
+    public Icon getDisabledIcon() {
             return null;
 	}
 
-	public String getDescription() {
+    public String getDescription() {
             return RUN_WITH_PERL_6_PROFILER;
 	}
 
-	@NotNull
-	public String getActionName() {
+    @NotNull
+    public String getActionName() {
             return RUN_WITH_PERL_6_PROFILER1;
 	}
 
-	@NotNull
-	public String getId() {
+    @NotNull
+    public String getId() {
             return RUN_WITH_PERL_6_PROFILER;
 	}
 
-	@NotNull
-	public String getStartActionText() {
+    @NotNull
+    public String getStartActionText() {
             return RUN_WITH_PERL_6_PROFILER;
         }
 
-	public String getContextActionId() {
-            // HACK: ExecutorRegistryImpl expects this to be non-null, but we don't want any context actions for every file
-            return getId() + " context-action-does-not-exist";
-	}
+    public String getContextActionId() {
+        // HACK: ExecutorRegistryImpl expects this to be non-null, but we don't want any context actions for every file
+        return getId() + " context-action-does-not-exist";
+    }
 
-	public String getHelpId() {
+    public String getHelpId() {
             return null;
         }
 }
