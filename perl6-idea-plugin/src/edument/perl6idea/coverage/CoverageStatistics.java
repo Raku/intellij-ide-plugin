021e507bc59eb681f6b8438302104d5f03bcfc3f
Jonathan Worthington <jnthn@jnthn.net>
2019-02-17 13:58:34 +0100

    Clean up copyright notice, dead code
    The notice was added automatically due to a misconfiguration.

--- perl6-idea-plugin/src/edument/perl6idea/coverage/CoverageStatistics.java
+++ perl6-idea-plugin/src/edument/perl6idea/coverage/CoverageStatistics.java
@@ -1,11 +1,6 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package edument.perl6idea.coverage;
 
-import java.text.DecimalFormat;
-
 public class CoverageStatistics {
-    private static final DecimalFormat percentFormat = new DecimalFormat("###.0");
-
     private int coveredLines;
     private int coverableLines;
 
