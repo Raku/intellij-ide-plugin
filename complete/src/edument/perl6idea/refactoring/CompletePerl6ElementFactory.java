ab445af94f555bc7dd8a83b623e8ef93fed39aa5
Altai-man <altay-man@mail.ru>
2019-04-25 18:24:21 +0300

    Add ability to stub infix and sub signature

--- complete/src/edument/perl6idea/refactoring/CompletePerl6ElementFactory.java
+++ complete/src/edument/perl6idea/refactoring/CompletePerl6ElementFactory.java
@@ -1,8 +1,8 @@
 package edument.perl6idea.refactoring;
 
 import com.intellij.openapi.project.Project;
-import edument.perl6idea.psi.Perl6ElementFactory;
-import edument.perl6idea.psi.Perl6Statement;
+import com.intellij.psi.PsiElement;
+import edument.perl6idea.psi.*;
 
 import java.util.List;
 import java.util.StringJoiner;
@@ -29,5 +29,25 @@ public class CompletePerl6ElementFactory extends Perl6ElementFactory {
                              data.name,
                              data.formSignature(true),
                              data.containsExpression ? "" : ";");
+    }
+
+    public static Perl6InfixApplication createInfixApplication(Project project, List<PsiElement> parts) {
+        return produceElement(project, getInfixApplicationText(parts), Perl6InfixApplication.class);
+    }
+
+    private static String getInfixApplicationText(List<PsiElement> parts) {
+        StringJoiner infix = new StringJoiner(", ");
+        parts.stream().map(PsiElement::getText).forEach(infix::add);
+        return infix.toString() + ";";
+    }
+
+    public static Perl6Signature createRoutineSignature(Project project, List<Perl6Parameter> parameters) {
+        return produceElement(project, createRoutineSignatureText(parameters), Perl6Signature.class);
+    }
+
+    private static String createRoutineSignatureText(List<Perl6Parameter> parameters) {
+        StringJoiner signature = new StringJoiner(", ");
+        parameters.stream().map(PsiElement::getText).forEach(signature::add);
+        return "sub foo(" + signature.toString() + ") {}";
     }
 }
