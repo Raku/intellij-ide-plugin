3e89a02722aa174430c66417ab1bdfec2a7e5867
Altai-man <altay-man@mail.ru>
2019-04-25 18:23:04 +0300

    Add methods to work with Perl6Infix values

--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6InfixImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6InfixImpl.java
@@ -2,11 +2,30 @@ package edument.perl6idea.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import edument.perl6idea.psi.Perl6Infix;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class Perl6InfixImpl extends ASTWrapperPsiElement implements Perl6Infix {
     public Perl6InfixImpl(@NotNull ASTNode node) {
         super(node);
+    }
+
+    @Nullable
+    @Override
+    public PsiElement getLeftSide() {
+        return skipWhitespacesBackward();
+    }
+
+    @Nullable
+    @Override
+    public PsiElement getRightSide() {
+        return skipWhitespacesForward();
+    }
+
+    @Override
+    public String getOperator() {
+        return getText();
     }
 }
