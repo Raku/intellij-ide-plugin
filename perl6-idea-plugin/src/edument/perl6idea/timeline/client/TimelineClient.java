8e0ceabb76876b7f3870073584481d4833cdb0e8
Jonathan Worthington <jnthn@jnthn.net>
2019-05-29 19:45:09 +0200

    Implement timeline protocol handshake
    Also fix a regression with the timeout executor cleanup.

--- perl6-idea-plugin/src/edument/perl6idea/timeline/client/TimelineClient.java
+++ perl6-idea-plugin/src/edument/perl6idea/timeline/client/TimelineClient.java
@@ -4,9 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.codec.Charsets;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -16,8 +16,6 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class TimelineClient {
-    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-
     private final String host;
     private final int port;
     private TimelineEventListener listener;
@@ -38,11 +36,14 @@ public class TimelineClient {
 
     private CompletableFuture<AsynchronousSocketChannel> establishConnection() {
         CompletableFuture<AsynchronousSocketChannel> outcome = new CompletableFuture<>();
-        attemptConnection(outcome, new LinkedList<>(Arrays.asList(0.0, 0.5, 0.5, 1.0, 2.0, 2.0, 4.0, 4.0, 4.0, 4.0)));
+        attemptConnection(outcome, Executors.newScheduledThreadPool(1),
+                          new LinkedList<>(Arrays.asList(0.0, 0.5, 0.5, 1.0, 2.0, 2.0, 4.0, 4.0, 4.0, 4.0)));
         return outcome;
     }
 
-    private void attemptConnection(CompletableFuture<AsynchronousSocketChannel> result, LinkedList<Double> backoff) {
+    private void attemptConnection(CompletableFuture<AsynchronousSocketChannel> result,
+                                   ScheduledExecutorService executor,
+                                   LinkedList<Double> backoff) {
         try {
             InetSocketAddress address = new InetSocketAddress(host, port);
             AsynchronousSocketChannel conn = AsynchronousSocketChannel.open();
@@ -55,70 +56,89 @@ public class TimelineClient {
 
                 @Override
                 public void failed(Throwable exc, Object attachment) {
-                    retryOrFail(result, backoff, exc);
+                    retryOrFail(result, backoff, executor, exc);
                 }
             });
         }
         catch (IOException e) {
-            retryOrFail(result, backoff, e);
+            retryOrFail(result, backoff, executor, e);
         }
     }
 
-    private void retryOrFail(CompletableFuture<AsynchronousSocketChannel> result, LinkedList<Double> backoff, Throwable e) {
+    private void retryOrFail(CompletableFuture<AsynchronousSocketChannel> result,
+                             LinkedList<Double> backoff,
+                             ScheduledExecutorService executor,
+                             Throwable e) {
         if (backoff.isEmpty()) {
             result.completeExceptionally(e);
             executor.shutdown();
         }
         else {
             double delay = backoff.removeFirst();
-            executor.schedule(() -> attemptConnection(result, backoff), (long)(delay * 1000), TimeUnit.MILLISECONDS);
+            executor.schedule(() -> attemptConnection(result, Executors.newScheduledThreadPool(1), backoff), (long)(delay * 1000), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private static class ConnectionState {
+        private final AsynchronousSocketChannel connection;
+        public final ByteBuffer outstanding = ByteBuffer.allocate(1024 * 1024);
+        public final TimelineEventListener listener;
+        public boolean handshook = false;
+
+        public ConnectionState(AsynchronousSocketChannel connection, TimelineEventListener listener) {
+            this.connection = connection;
+            this.listener = listener;
         }
     }
 
     private void dispatchEvents(AsynchronousSocketChannel conn, TimelineEventListener listener) {
-        ByteBuffer outstanding = ByteBuffer.allocate(1024 * 1024);
-        read(conn, outstanding, listener);
+        conn.write(ByteBuffer.wrap("{ \"min\": 1, \"max\": 1 }\n".getBytes(Charsets.US_ASCII)));
+        read(new ConnectionState(conn, listener));
     }
 
-    private void read(AsynchronousSocketChannel conn, ByteBuffer outstanding, TimelineEventListener listener) {
+    private void read(ConnectionState state) {
         ByteBuffer buffer = ByteBuffer.allocate(32768);
-        conn.read(buffer, null, new CompletionHandler<Integer, Object>() {
+        state.connection.read(buffer, null, new CompletionHandler<Integer, Object>() {
             @Override
             public void completed(Integer bytesRead, Object attachment) {
                 if (bytesRead >= 0) {
                     buffer.flip();
-                    outstanding.put(buffer);
-                    processBuffer(outstanding, listener);
-                    read(conn, outstanding, listener);
+                    state.outstanding.put(buffer);
+                    processBuffer(state);
+                    read(state);
                 }
             }
 
             @Override
             public void failed(Throwable exc, Object attachment) {
                 listener.onError(exc);
-                try {
-                    conn.close();
-                }
-                catch (IOException e) {
-                    // Already reported an error, so nothing to do.
-                }
+                closeConnection(state);
             }
         });
     }
 
-    private void processBuffer(ByteBuffer outstanding, TimelineEventListener listener) {
+    private void closeConnection(ConnectionState state) {
+        try {
+            state.connection.close();
+        }
+        catch (IOException e) {
+            // Already reported an error, so nothing to do.
+        }
+    }
+
+    private void processBuffer(ConnectionState state) {
         boolean again = true;
         while (again) {
-            int end = outstanding.position();
+            int end = state.outstanding.position();
             again = false;
             for (int i = 0; i < end; i++) {
-                if (outstanding.get(i) == '\n') {
+                if (state.outstanding.get(i) == '\n') {
                     byte[] message = new byte[i + 1];
-                    outstanding.position(0);
-                    outstanding.get(message);
-                    processMessage(message, listener);
-                    outstanding.limit(end);
-                    outstanding.compact();
+                    state.outstanding.position(0);
+                    state.outstanding.get(message);
+                    processMessage(message, state);
+                    state.outstanding.limit(end);
+                    state.outstanding.compact();
                     again = true;
                     break;
                 }
@@ -127,25 +147,41 @@ public class TimelineClient {
     }
 
     private static final Map<String, Object> EMPTY_DATA = new HashMap<>();
-    private void processMessage(byte[] message, TimelineEventListener listener) {
+    private void processMessage(byte[] message, ConnectionState state) {
         try {
             String decoded = new String(message, "UTF-8");
             JsonObject json = new JsonParser().parse(decoded).getAsJsonObject();
-            String module = json.get("m").getAsString();
-            String category = json.get("c").getAsString();
-            String name = json.get("n").getAsString();
-            int kind = json.get("k").getAsInt();
-            double timestamp = json.get("t").getAsDouble();
-            int id = json.has("i") ? json.get("i").getAsInt() : 0;
-            int parent = json.has("p") ? json.get("p").getAsInt() : 0;
-            Type type = new TypeToken<Map<String, Object>>(){}.getType();
-            Map<String, Object> data = json.has("d")
-                    ? new Gson().fromJson(json.get("d"), type)
-                    : EMPTY_DATA;
-            listener.onEvent(new ClientEvent(module, category, name, kind, timestamp, id, parent, data));
+            if (state.handshook) {
+                String module = json.get("m").getAsString();
+                String category = json.get("c").getAsString();
+                String name = json.get("n").getAsString();
+                int kind = json.get("k").getAsInt();
+                double timestamp = json.get("t").getAsDouble();
+                int id = json.has("i") ? json.get("i").getAsInt() : 0;
+                int parent = json.has("p") ? json.get("p").getAsInt() : 0;
+                Type type = new TypeToken<Map<String, Object>>() {
+                }.getType();
+                Map<String, Object> data = json.has("d")
+                                           ? new Gson().fromJson(json.get("d"), type)
+                                           : EMPTY_DATA;
+                state.listener.onEvent(new ClientEvent(module, category, name, kind, timestamp, id, parent, data));
+            }
+            else {
+                if (json.has("err")) {
+                    state.listener.onError(new Exception(json.get("err").getAsString()));
+                    closeConnection(state);
+                }
+                else if (!json.has("ver")) {
+                    state.listener.onError(new Exception("Server handshake failed; missing ver"));
+                    closeConnection(state);
+                }
+                else {
+                    state.handshook = true;
+                }
+            }
         }
         catch (Exception e) {
-            listener.onError(e);
+            state.listener.onError(e);
         }
     }
 }
