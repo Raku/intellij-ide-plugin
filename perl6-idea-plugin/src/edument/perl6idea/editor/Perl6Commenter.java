79e96f4c446e961e2bdae49f337da5f930ddef7c
Altai-man <altay-man@mail.ru>
2019-07-05 17:17:47 +0300

    Implement block commenter

--- perl6-idea-plugin/src/edument/perl6idea/editor/Perl6Commenter.java
+++ perl6-idea-plugin/src/edument/perl6idea/editor/Perl6Commenter.java
@@ -1,9 +1,18 @@
 package edument.perl6idea.editor;
 
 import com.intellij.lang.Commenter;
+import com.intellij.lang.CustomUncommenter;
+import com.intellij.openapi.util.Couple;
+import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class Perl6Commenter implements Commenter {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Perl6Commenter implements Commenter, CustomUncommenter {
     @Nullable
     @Override
     public String getLineCommentPrefix() {
@@ -13,13 +22,13 @@ public class Perl6Commenter implements Commenter {
     @Nullable
     @Override
     public String getBlockCommentPrefix() {
-        return "#";
+        return "#`[";
     }
 
     @Nullable
     @Override
     public String getBlockCommentSuffix() {
-        return null;
+        return "]";
     }
 
     @Nullable
@@ -32,5 +41,25 @@ public class Perl6Commenter implements Commenter {
     @Override
     public String getCommentedBlockCommentSuffix() {
         return null;
+    }
+
+    @Nullable
+    @Override
+    public TextRange findMaximumCommentedRange(@NotNull CharSequence text) {
+        String charStream = text.toString().trim();
+        if (charStream.startsWith("#`[") && charStream.endsWith("]"))
+            return new TextRange(0, text.length() - 1);
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Collection<? extends Couple<TextRange>> getCommentRangesToDelete(@NotNull CharSequence sequence) {
+        List<Couple<TextRange>> commentRanges = new ArrayList<>();
+        String text = sequence.toString();
+        int start = text.indexOf("#`[");
+        int end = text.lastIndexOf("]");
+        commentRanges.add(new Couple<>(new TextRange(start, start + 4), new TextRange(end, end + 2)));
+        return commentRanges;
     }
 }
