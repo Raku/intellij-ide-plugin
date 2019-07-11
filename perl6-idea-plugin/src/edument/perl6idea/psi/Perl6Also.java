3d62f0da487056e4f8e56491c4a8a3c0c0a13a82
Jonathan Worthington <jnthn@jnthn.net>
2019-07-12 00:51:35 +0200

    Perl6Also should implement Perl6PsiElement

--- perl6-idea-plugin/src/edument/perl6idea/psi/Perl6Also.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/Perl6Also.java
@@ -1,8 +1,7 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package edument.perl6idea.psi;
 
 import edument.perl6idea.psi.symbols.Perl6MOPSymbolContributor;
 
-public interface Perl6Also extends Perl6MOPSymbolContributor {
+public interface Perl6Also extends Perl6PsiElement, Perl6MOPSymbolContributor {
     Perl6Trait getTrait();
 }
