19232abe7032f80bf5cb2abc7ba10476196d4415
Altai-man <altay-man@mail.ru>
2019-07-21 14:48:54 +0300

    Enable enum resolution

--- perl6-idea-plugin/src/edument/perl6idea/psi/Perl6Enum.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/Perl6Enum.java
@@ -3,11 +3,12 @@ package edument.perl6idea.psi;
 import com.intellij.psi.StubBasedPsiElement;
 import edument.perl6idea.psi.stub.Perl6EnumStub;
 import edument.perl6idea.psi.stub.index.Perl6IndexableType;
+import edument.perl6idea.psi.symbols.Perl6LexicalSymbolContributor;
 
 import java.util.List;
 
 public interface Perl6Enum extends Perl6PsiDeclaration, StubBasedPsiElement<Perl6EnumStub>,
-                                   Perl6IndexableType {
+                                   Perl6IndexableType, Perl6LexicalSymbolContributor {
     String getEnumName();
     List<String> getEnumValues();
 }
