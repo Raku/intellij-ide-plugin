6f7357099dd356f26508e69ab6f8b2ab71e3f621
Altai-man <altay-man@mail.ru>
2019-09-18 18:40:17 +0300

    Add CommaFixtureTest case that takes care of caching

---
+++ perl6-idea-plugin/tests/edument/perl6idea/CommaFixtureTestCase.java
@@ -0,0 +1,44 @@
+package edument.perl6idea;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import edument.perl6idea.sdk.Perl6SdkType;
+import org.jetbrains.annotations.NotNull;
+
+public class CommaFixtureTestCase extends LightPlatformCodeInsightFixtureTestCase {
+    private Sdk testSdk;
+
+    @NotNull
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new Perl6LightProjectDescriptor();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "perl6-idea-plugin/testData/completion";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ApplicationManager.getApplication().runWriteAction(() -> {
+            String homePath = Perl6SdkType.getInstance().suggestHomePath();
+            assertNotNull("Found a perl6 in path to use in tests", homePath);
+            testSdk = SdkConfigurationUtil.createAndAddSDK(homePath, Perl6SdkType.getInstance());
+            ProjectRootManager.getInstance(myModule.getProject()).setProjectSdk(testSdk);
+        });
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        Perl6SdkType sdkType = (Perl6SdkType)testSdk.getSdkType();
+        sdkType.invalidateFileCache();
+        SdkConfigurationUtil.removeSdk(testSdk);
+        super.tearDown();
+    }
+}
