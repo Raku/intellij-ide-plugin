7ad8c86ff4b7f467741d87a29cbb333410338b5e
Altai-man <altay-man@mail.ru>
2019-11-15 19:24:15 +0200

    Stub usage of external libraries for project view

--- perl6-idea-plugin/src/edument/perl6idea/structureView/Perl6ExternalLibrariesNode.java
+++ perl6-idea-plugin/src/edument/perl6idea/structureView/Perl6ExternalLibrariesNode.java
@@ -7,12 +7,15 @@ import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.PlatformIcons;
-import edument.perl6idea.metadata.Perl6MetaDataComponent;
+import com.intellij.openapi.roots.LibraryOrderEntry;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderEntry;
+import com.intellij.openapi.roots.libraries.Library;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Perl6ExternalLibrariesNode extends ExternalLibrariesNode {
     public Perl6ExternalLibrariesNode(@NotNull Project project,
@@ -25,31 +28,22 @@ public class Perl6ExternalLibrariesNode extends ExternalLibrariesNode {
     public Collection<? extends AbstractTreeNode> getChildren() {
         if (myProject == null)
             return new ArrayList<>();
+        List<AbstractTreeNode> children = new ArrayList<>();
 
-        Collection<AbstractTreeNode<String>> c = new ArrayList<>();
-        Module[] projectModules = ModuleManager.getInstance(myProject).getModules();
-        for (Module module : projectModules) {
-            Perl6MetaDataComponent metaData = module.getComponent(Perl6MetaDataComponent.class);
-            metaData.getDepends(false).forEach(name -> addName(name, c));
-            metaData.getBuildDepends(false).forEach(name -> addName(name, c));
-            metaData.getTestDepends(false).forEach(name -> addName(name, c));
+        Module[] modules = ModuleManager.getInstance(myProject).getModules();
+        for (Module module : modules) {
+            final ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+            final OrderEntry[] orderEntries = moduleRootManager.getOrderEntries();
+            for (final OrderEntry orderEntry : orderEntries) {
+                if (orderEntry instanceof LibraryOrderEntry) {
+                    final LibraryOrderEntry libraryOrderEntry = (LibraryOrderEntry)orderEntry;
+                    final Library library = libraryOrderEntry.getLibrary();
+                    if (library == null) continue;
+                    children.add(new Perl6LibraryNode(myProject, library, getSettings()));
+                }
+            }
         }
-        return c;
-    }
 
-    private void addName(String name, Collection<AbstractTreeNode<String>> c) {
-        c.add(new AbstractTreeNode<String>(myProject, name) {
-            @Override
-            protected void update(PresentationData presentation) {
-                presentation.setPresentableText(name);
-                presentation.setIcon(PlatformIcons.LIBRARY_ICON);
-            }
-
-            @NotNull
-            @Override
-            public Collection<? extends AbstractTreeNode> getChildren() {
-                return new ArrayList<>();
-            }
-        });
+        return children;
     }
 }
