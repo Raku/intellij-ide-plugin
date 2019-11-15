4362b380f2be610e60778048daf70a7268e18c0b
Altai-man <altay-man@mail.ru>
2019-11-15 19:22:48 +0200

    Add support of compunit packages for Perl6FileSystem

--- perl6-idea-plugin/src/edument/perl6idea/vfs/Perl6FileSystem.java
+++ perl6-idea-plugin/src/edument/perl6idea/vfs/Perl6FileSystem.java
@@ -1,16 +1,32 @@
 package edument.perl6idea.vfs;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileListener;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.vfs.VirtualFileSystem;
+import com.intellij.testFramework.LightVirtualFile;
+import edument.perl6idea.utils.Perl6CommandLine;
+import edument.perl6idea.utils.Perl6Utils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class Perl6FileSystem extends VirtualFileSystem {
     public static final String PROTOCOL = "raku";
+    private static final Logger LOG = Logger.getInstance(Perl6FileSystem.class);
+    private Map<String, Map<String, VirtualFile>> cache = new ConcurrentHashMap<>();
 
     public static Perl6FileSystem getInstance() {
         return (Perl6FileSystem)VirtualFileManager.getInstance().getFileSystem(PROTOCOL);
@@ -22,10 +38,42 @@ public class Perl6FileSystem extends VirtualFileSystem {
         return PROTOCOL;
     }
 
+    public Collection<VirtualFile> findFilesByPath(@NotNull Project project, @NotNull String path) {
+        return cache.computeIfAbsent(path, (name) -> {
+            try {
+                Map<String, VirtualFile> packageFiles = new ConcurrentHashMap<>();
+                File locateScript = Perl6Utils.getResourceAsFile("zef/locate.p6");
+                if (locateScript == null) {
+                    throw new ExecutionException("File locator is called with corrupted resources bundle, aborting");
+                }
+                Perl6CommandLine cmd = new Perl6CommandLine(project);
+                cmd.setWorkDirectory(System.getProperty("java.io.tmpdir"));
+                cmd.addParameters(locateScript.getPath());
+                cmd.addParameters(path);
+                for (String compUnit : cmd.executeAndRead()) {
+                    String[] pieces = compUnit.split("=");
+                    if (pieces.length == 2) {
+                        String fileContents = Files.lines(Paths.get(pieces[1]), StandardCharsets.UTF_8).collect(Collectors.joining("\n"));
+                        ;
+                        new File(pieces[1]);
+                        VirtualFile file = new LightVirtualFile(pieces[0] + ".pm6", fileContents);
+                        packageFiles.put(pieces[0], file);
+                    }
+                }
+
+                //return new Perl6LibraryVirtualFile(this, path, path, packageFiles.values().toArray(VirtualFile.EMPTY_ARRAY));
+                return packageFiles;
+            }
+            catch (ExecutionException | IOException e) {
+                LOG.warn(e);
+            }
+            return new ConcurrentHashMap<>();
+        }).values();
+    }
+
     @Nullable
     @Override
     public VirtualFile findFileByPath(@NotNull String path) {
-        // Resolve to in-memory file here
         return null;
     }
 
