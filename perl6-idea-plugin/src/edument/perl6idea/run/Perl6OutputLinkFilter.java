9c68d39127afe23eee5f760345c66e5377d2c250
Altai-man <altay-man@mail.ru>
2019-12-04 16:00:44 +0200

    Use indexing in output link filter
    The heuristic with using project's base path as prefix predicate is cheap,
    but at the same time:
    * It does not work with local names
    * It uses deprecated mechanism of `getBasePath` which we want eventually get rid of,
      so one place less to look for.
    Newer heuristic does not have the cons above and is not really expensive either.

--- perl6-idea-plugin/src/edument/perl6idea/run/Perl6OutputLinkFilter.java
+++ perl6-idea-plugin/src/edument/perl6idea/run/Perl6OutputLinkFilter.java
@@ -5,9 +5,13 @@ import com.intellij.execution.filters.OpenFileHyperlinkInfo;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,33 +19,37 @@ import java.util.regex.Pattern;
 public class Perl6OutputLinkFilter implements Filter {
     private static final Pattern FILE_PATTERN = Pattern.compile("\\s*?in (?:\\S+)(?: \\S+?)? +at (\\S+)(?: \\(\\S*\\))? line (\\d+)");
     private final Project myProject;
-    private VirtualFile baseDir;
-    private String baseDirPath;
 
     public Perl6OutputLinkFilter(Project project) {
         myProject = project;
-        baseDir = project.getBaseDir();
-        baseDirPath = baseDir.getCanonicalPath();
     }
 
     @Nullable
     @Override
     public Result applyFilter(@NotNull String line, int entireLength) {
-        if (baseDirPath == null) return null;
+
         int startPoint = entireLength - line.length();
         Matcher matcher = FILE_PATTERN.matcher(line);
 
-        if (matcher.find() && matcher.group(1).startsWith(baseDirPath)) {
-            VirtualFile file = baseDir.getFileSystem().findFileByPath(matcher.group(1));
-            if (file == null)
-                return null;
-            OpenFileDescriptor fileDescriptor =
-                new OpenFileDescriptor(myProject, file,
-                                       Integer.parseInt(matcher.group(2)) - 1, 0);
-            ResultItem item = new ResultItem(startPoint + matcher.start(1),
-                                             startPoint + matcher.end(1),
-                                             new OpenFileHyperlinkInfo(fileDescriptor));
-            return new Result(Collections.singletonList(item));
+        if (!matcher.find())
+            return null;
+
+        String pathPart = matcher.group(1);
+        Collection<VirtualFile> files =
+            FilenameIndex.getVirtualFilesByName(myProject, Paths.get(pathPart).getFileName().toString(), GlobalSearchScope.allScope(myProject));
+        if (files.isEmpty())
+            return null;
+
+        for (VirtualFile file : files) {
+            if (file.getPath().endsWith(pathPart)) {
+                OpenFileDescriptor fileDescriptor =
+                    new OpenFileDescriptor(myProject, file,
+                                           Integer.parseInt(matcher.group(2)) - 1, 0);
+                ResultItem item = new ResultItem(startPoint + matcher.start(1),
+                                                 startPoint + matcher.end(1),
+                                                 new OpenFileHyperlinkInfo(fileDescriptor));
+                return new Result(Collections.singletonList(item));
+            }
         }
         return null;
     }
