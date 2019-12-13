f454cfbe54b7421bd77243cfbee7f897e5d118f0
Altai-man <altay-man@mail.ru>
2019-12-13 23:36:57 +0200

    Consider reference-based as well as direct resolve
    It allows us consider regex-driven vars during
    guessing if they are declared or not.

--- perl6-idea-plugin/src/edument/perl6idea/annotation/UndeclaredVariableAnnotator.java
+++ perl6-idea-plugin/src/edument/perl6idea/annotation/UndeclaredVariableAnnotator.java
@@ -4,6 +4,7 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReference;
 import com.intellij.psi.util.PsiTreeUtil;
 import edument.perl6idea.psi.*;
 import edument.perl6idea.psi.symbols.Perl6Symbol;
@@ -20,22 +21,22 @@ public class UndeclaredVariableAnnotator implements Annotator {
             return;
         if (element.getParent() instanceof Perl6VariableDecl)
             return;
-        final Perl6Variable ref = (Perl6Variable) element;
-        String variableName = ref.getVariableName();
+        final Perl6Variable variable = (Perl6Variable)element;
+        String variableName = variable.getVariableName();
         if (variableName == null)
             return;
 
         // Check for $=finish section
         if (Perl6Variable.getTwigil(variableName) == '=' && variableName.equals("$=finish")) {
             PsiElement list = PsiTreeUtil.getChildOfType(
-              PsiTreeUtil.getParentOfType(element, PsiFile.class),
-              Perl6StatementList.class);
+                PsiTreeUtil.getParentOfType(element, PsiFile.class),
+                Perl6StatementList.class);
             if (list == null) return;
             PsiElement maybeFinish = PsiTreeUtil.findChildOfType(list, PodBlockFinish.class);
             if (maybeFinish == null)
                 holder.createErrorAnnotation(
-                  element,
-                  "There is no =finish section in this file");
+                    element,
+                    "There is no =finish section in this file");
         }
 
         // We only check usual variables in this annotator
@@ -54,20 +55,23 @@ public class UndeclaredVariableAnnotator implements Annotator {
             return;
 
         // Otherwise, try to resolve it.
-        Perl6Symbol resolved = ref.resolveLexicalSymbol(Perl6SymbolKind.Variable, variableName);
-        if (resolved == null) {
-            // Straight resolution failure
-            holder.createErrorAnnotation(element,
-                String.format("Variable %s is not declared", variableName));
+        Perl6Symbol symbol = variable.resolveLexicalSymbol(Perl6SymbolKind.Variable, variableName);
+        if (symbol == null) {
+            PsiReference reference = variable.getReference();
+            assert reference != null;
+            PsiElement resolved = reference.resolve();
+            if (resolved == null) {
+                // Straight resolution failure
+                holder.createErrorAnnotation(element,
+                                             String.format("Variable %s is not declared", variableName));
+            }
         }
         else {
-            // May not be available yet
             if (Perl6Variable.getSigil(variableName) != '&') {
-                PsiElement psi = resolved.getPsi();
-                if (psi != null && psi.getContainingFile() == ref.getContainingFile() &&
-                        psi.getTextOffset() > ref.getTextOffset())
+                PsiElement psi = symbol.getPsi();
+                if (psi != null && psi.getContainingFile() == variable.getContainingFile() && psi.getTextOffset() > variable.getTextOffset())
                     holder.createErrorAnnotation(element,
-                        String.format("Variable %s is not declared in this scope yet", variableName));
+                                                 String.format("Variable %s is not declared in this scope yet", variableName));
             }
         }
     }
