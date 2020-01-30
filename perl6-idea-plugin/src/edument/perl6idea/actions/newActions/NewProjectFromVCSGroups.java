b11ccc8eddf2dcb5c2ef040476a3ec589da814a8
Altai-man <alexander.kiryuhin@gmail.com>
2020-01-30 18:45:04 +0200

    Remove now redundant action class

--- perl6-idea-plugin/src/edument/perl6idea/actions/newActions/NewProjectFromVCSGroups.java
+++
@@ -1,6 +0,0 @@
-package edument.perl6idea.actions.newActions;
-
-import com.intellij.openapi.wm.impl.welcomeScreen.GetFromVersionControlAction;
-
-public class NewProjectFromVCSGroups extends GetFromVersionControlAction {
-}
