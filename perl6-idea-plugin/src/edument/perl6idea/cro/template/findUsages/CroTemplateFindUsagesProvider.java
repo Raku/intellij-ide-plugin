d0560847c920f5c3a876ccc733392d168ebd2e8d
Jonathan Worthington <jnthn@jnthn.net>
2020-02-06 15:46:06 +0100

    Basic find usages provider for Cro templates

---
+++ perl6-idea-plugin/src/edument/perl6idea/cro/template/findUsages/CroTemplateFindUsagesProvider.java
@@ -0,0 +1,49 @@
+package edument.perl6idea.cro.template.findUsages;
+
+import com.intellij.lang.HelpID;
+import com.intellij.lang.findUsages.FindUsagesProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import edument.perl6idea.cro.template.psi.CroTemplateMacro;
+import edument.perl6idea.cro.template.psi.CroTemplateParameter;
+import edument.perl6idea.cro.template.psi.CroTemplateSub;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CroTemplateFindUsagesProvider implements FindUsagesProvider {
+    @Override
+    public boolean canFindUsagesFor(@NotNull PsiElement element) {
+        return element instanceof PsiNamedElement;
+    }
+
+    @Nullable
+    @Override
+    public String getHelpId(@NotNull PsiElement psiElement) {
+        return HelpID.FIND_OTHER_USAGES;
+    }
+
+    @NotNull
+    @Override
+    public String getType(@NotNull PsiElement element) {
+        if (element instanceof CroTemplateMacro)
+            return "Cro template macro";
+        if (element instanceof CroTemplateSub)
+            return "Cro template sub";
+        if (element instanceof CroTemplateParameter)
+            return "Cro template parameter";
+        return "Cro template element";
+    }
+
+    @NotNull
+    @Override
+    public String getDescriptiveName(@NotNull PsiElement element) {
+        String name = ((PsiNamedElement)element).getName();
+        return name == null ? "" : name;
+    }
+
+    @NotNull
+    @Override
+    public String getNodeText(@NotNull PsiElement element, boolean useFullName) {
+        return getDescriptiveName(element);
+    }
+}
