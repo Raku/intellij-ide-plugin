6a599710bade3950a3037899d32f0ece8edbb124
Jonathan Worthington <jnthn@jnthn.net>
2020-04-28 01:03:11 +0200

    Default grammar preview to the right

--- perl6-idea-plugin/src/edument/perl6idea/grammar/RakuGrammarPreviewFactory.java
+++ perl6-idea-plugin/src/edument/perl6idea/grammar/RakuGrammarPreviewFactory.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
@@ -30,6 +31,7 @@ public class RakuGrammarPreviewFactory implements ToolWindowFactory {
     @Override
     public void init(ToolWindow window) {
         window.setIcon(Perl6Icons.CAMELIA_13x13);
+        window.setAnchor(ToolWindowAnchor.RIGHT, null);
     }
 
     @Override
