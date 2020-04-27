4077cdef0e5f0fbc38220e3e95af261558c88422
Altai-man <alexander.kiryuhin@gmail.com>
2020-04-27 16:15:47 +0300

    Add grammar preview tool window

---
+++ perl6-idea-plugin/src/edument/perl6idea/grammar/RakuGrammarPreviewFactory.java
@@ -0,0 +1,44 @@
+package edument.perl6idea.grammar;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import edument.perl6idea.Perl6Icons;
+import edument.perl6idea.module.Perl6ModuleType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class RakuGrammarPreviewFactory implements ToolWindowFactory {
+    public static final String PREVIEW_WINDOW_ID = "Raku Grammar Preview";
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        Module[] modules = ModuleManager.getInstance(project).getModules();
+        for (Module module : modules) {
+            if (Objects.equals(Perl6ModuleType.ID, module.getModuleTypeName()))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void init(ToolWindow window) {
+        window.setIcon(Perl6Icons.CAMELIA_13x13);
+    }
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        if (ApplicationManager.getApplication().isUnitTestMode()) return;
+        RakuGrammarPreviewer previewer = new RakuGrammarPreviewer();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(previewer, "", false);
+        content.setCloseable(false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
---
+++ perl6-idea-plugin/src/edument/perl6idea/grammar/RakuGrammarPreviewer.java
@@ -0,0 +1,6 @@
+package edument.perl6idea.grammar;
+
+import javax.swing.*;
+
+public class RakuGrammarPreviewer extends JPanel {
+}
