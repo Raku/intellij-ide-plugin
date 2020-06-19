dc771f00d8f2e3d70ba7bd4835a1c8c932330f23
Altai-man <alexander.kiryuhin@gmail.com>
2020-06-19 12:52:03 +0300

    Add line markers to test files only

--- complete/src/edument/perl6idea/testing/Perl6TestLineMarkerContributor.java
+++ complete/src/edument/perl6idea/testing/Perl6TestLineMarkerContributor.java
@@ -7,10 +7,12 @@ import edument.perl6idea.psi.Perl6File;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+
 public class Perl6TestLineMarkerContributor extends RunLineMarkerContributor {
     @Override
     public @Nullable Info getInfo(@NotNull PsiElement element) {
-        if (element instanceof Perl6File)
+        if (element instanceof Perl6File && Arrays.asList("t", "t6", "rakutest").contains(((Perl6File)element).getVirtualFile().getExtension()))
             return withExecutorActions(AllIcons.RunConfigurations.TestState.Run);
         return null;
     }
