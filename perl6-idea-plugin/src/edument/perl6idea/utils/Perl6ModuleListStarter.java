d138dbf7680dd6f13c4df57a3d4a83d89942f972
Altai-man <alexander.kiryuhin@gmail.com>
2020-06-25 17:29:05 +0300

    Initialize component before static usage

--- perl6-idea-plugin/src/edument/perl6idea/utils/Perl6ModuleListStarter.java
+++ perl6-idea-plugin/src/edument/perl6idea/utils/Perl6ModuleListStarter.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
 public class Perl6ModuleListStarter implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
+        // Create service if not yet
+        project.getService(Perl6ModuleListFetcher.class);
         Perl6ModuleListFetcher.refreshModules(project);
     }
 }
