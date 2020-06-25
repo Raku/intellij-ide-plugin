1665795cca2436e98e8fddca8c83109cd1079c85
Altai-man <alexander.kiryuhin@gmail.com>
2020-06-25 12:56:42 +0300

    Rename class to match style

--- perl6-idea-plugin/src/edument/perl6idea/intention/SplitDeclarationIntention.java
+++ perl6-idea-plugin/src/edument/perl6idea/intention/SplitDeclarationIntention.java
@@ -15,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
-public class SplitDeclaration extends PsiElementBaseIntentionAction implements IntentionAction {
+public class SplitDeclarationIntention extends PsiElementBaseIntentionAction implements IntentionAction {
     @Override
     public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
         // Prepare variables necessary for formatting
