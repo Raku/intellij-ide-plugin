bceda35418b91b7f3d087bcc598a9626e38e04d1
Altai-man <alexander.kiryuhin@gmail.com>
2020-07-12 17:25:47 +0300

    De-deprecate Comma test fixture

--- perl6-idea-plugin/tests/edument/perl6idea/Perl6LightProjectDescriptor.java
+++ perl6-idea-plugin/tests/edument/perl6idea/Perl6LightProjectDescriptor.java
@@ -1,6 +1,11 @@
 package edument.perl6idea;
 
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.RootModelProvider;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.LightProjectDescriptor;
@@ -8,15 +13,28 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Perl6LightProjectDescriptor extends LightProjectDescriptor {
     @Nullable
     @Override
     public VirtualFile createSourcesRoot(@NotNull Module module) {
-        VirtualFile projectRoot = module.getProject().getBaseDir();
-        assert projectRoot != null;
-        projectRoot.refresh(false, true);
-        VirtualFile srcRoot = doCreateSourceRoot(projectRoot, "lib");
+        ContentEntry[] entries = ModuleRootManager.getInstance(module).getContentEntries();
+        if (entries.length == 0) {
+            ModifiableRootModel rootModel = ModuleRootManager.getInstance(module).getModifiableModel();
+            String stringPath = ModuleUtilCore.getModuleDirPath(module);
+            ContentEntry entry = rootModel.addContentEntry(Paths.get(stringPath).toUri().toString());
+            entries = new ContentEntry[]{entry};
+            rootModel.commit();
+        }
+        else if (entries.length != 1) {
+            assert false;
+        }
+        VirtualFile moduleRoot = entries[0].getFile();
+        assert moduleRoot != null;
+        moduleRoot.refresh(false, true);
+        VirtualFile srcRoot = doCreateSourceRoot(moduleRoot, "lib");
         registerSourceRoot(module.getProject(), srcRoot);
         return srcRoot;
     }
@@ -30,7 +48,8 @@ public class Perl6LightProjectDescriptor extends LightProjectDescriptor {
                 child.delete(this);
             srcRoot = root.createChildDirectory(this, srcPath);
             cleanSourceRoot(srcRoot);
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
 
@@ -38,7 +57,7 @@ public class Perl6LightProjectDescriptor extends LightProjectDescriptor {
     }
 
     private void cleanSourceRoot(VirtualFile contentRoot) throws IOException {
-        LocalFileSystem vfs = (LocalFileSystem) contentRoot.getFileSystem();
+        LocalFileSystem vfs = (LocalFileSystem)contentRoot.getFileSystem();
         for (VirtualFile child : contentRoot.getChildren()) {
             if (vfs.exists(child))
                 child.delete(this);
