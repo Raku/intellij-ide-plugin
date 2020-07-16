8271b9947a9e5b84e69ebf26ae8ee88e0973d2e0
Altai-man <alexander.kiryuhin@gmail.com>
2020-07-16 18:09:50 +0300

    Init a service to listen to META changes on start

--- perl6-idea-plugin/src/edument/perl6idea/utils/Perl6ModuleListStarter.java
+++ perl6-idea-plugin/src/edument/perl6idea/utils/Perl6ModuleListStarter.java
@@ -1,7 +1,10 @@
 package edument.perl6idea.utils;
 
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import edument.perl6idea.metadata.Perl6MetaDataComponent;
 import org.jetbrains.annotations.NotNull;
 
 public class Perl6ModuleListStarter implements StartupActivity {
@@ -10,5 +13,9 @@ public class Perl6ModuleListStarter implements StartupActivity {
         // Create service if not yet
         project.getService(Perl6ModuleListFetcher.class);
         Perl6ModuleListFetcher.refreshModules(project);
+        // Initialize metadata listeners
+        Module[] modules = ModuleManager.getInstance(project).getModules();
+        for (Module module : modules)
+            module.getService(Perl6MetaDataComponent.class);
     }
 }
