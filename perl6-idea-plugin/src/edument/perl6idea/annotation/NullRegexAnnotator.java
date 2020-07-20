32533981e7ba023d30145cd5f7b2590100eeef92
Altai-man <alexander.kiryuhin@gmail.com>
2020-07-20 15:09:47 +0300

    Quickfixes for annotators

--- perl6-idea-plugin/src/edument/perl6idea/annotation/NullRegexAnnotator.java
+++ perl6-idea-plugin/src/edument/perl6idea/annotation/NullRegexAnnotator.java
@@ -5,16 +5,16 @@ import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.psi.PsiElement;
 import edument.perl6idea.annotation.fix.NullRegexFix;
-import edument.perl6idea.psi.Perl6Regex;
+import edument.perl6idea.psi.Perl6RegexDriver;
 import org.jetbrains.annotations.NotNull;
 
 public class NullRegexAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-        if (!(element instanceof Perl6Regex))
+        if (!(element instanceof Perl6RegexDriver))
             return;
         if (element.getText().isEmpty())
             holder.newAnnotation(HighlightSeverity.ERROR, "Empty regex is not allowed")
-                .range(element.getParent()).withFix(new NullRegexFix()).create();
+                .range(element).withFix(new NullRegexFix()).create();
     }
 }
