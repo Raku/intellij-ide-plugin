8dd407f96528e645a33656113e8636af747e81e0
Altai-man <alexander.kiryuhin@gmail.com>
2020-08-24 13:08:26 +0300

    Add `Mark directory as` actions to modify source roots

---
+++ perl6-idea-plugin/src/edument/perl6idea/actions/moduleRoot/MarkRakuSourceRootAction.java
@@ -0,0 +1,7 @@
+package edument.perl6idea.actions.moduleRoot;
+
+public class MarkRakuSourceRootAction extends RakuMarkRootActionBase {
+    public MarkRakuSourceRootAction() {
+        super(false);
+    }
+}
