f2f59b91b3c71eedd4b1bceb1d46ad105f26efa3
Jonathan Worthington <jnthn@jnthn.net>
2021-03-26 17:26:12 +0100

    Annotate duplicate conditions in if/with/else
    Some normalization is performed for Unicode vs. ASCII, commuting ops,
    ops that have an alternative where the operands are swapped, and to
    ensure whitespace and comments have no influence. The most likely thing
    this will catch, howerver, is copy-paste-forget.

---
+++ perl6-idea-plugin/src/edument/perl6idea/annotation/DuplicateConditionAnnotator.java
@@ -0,0 +1,117 @@
+package edument.perl6idea.annotation;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.containers.ContainerUtil;
+import edument.perl6idea.psi.Perl6IfStatement;
+import edument.perl6idea.psi.Perl6InfixApplication;
+import edument.perl6idea.psi.Perl6PsiElement;
+import edument.perl6idea.utils.Perl6OperatorUtils;
+import edument.perl6idea.utils.Perl6PsiUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class DuplicateConditionAnnotator implements Annotator {
+    private static final Set<String> COMMUTERS = ContainerUtil.set(
+            "==", "!=", "eq", "ne", "===", "cmp", "eqv", "=~=", "=:=", "(==)",
+            "+", "*", "&", "|");
+    private static final Map<String, String> UNINORM = new HashMap<>() {{
+        for (Pair<Character, String> op : ContainerUtil.zip(Perl6OperatorUtils.unicodeOperators, Perl6OperatorUtils.asciiOperators))
+            put(op.first.toString(), op.second);
+    }};
+    private static final Map<String, String> REVERSES = new HashMap<>() {{
+        put(">", "<");
+        put(">=", "<=");
+        put("gt", "lt");
+        put("ge", "le");
+        put("(<)", "(>)");
+        put("(<=)", "(>=)");
+        put("(<+)", "(>+)");
+    }};
+
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        // Make sure it's a if/with/elsif/orwith structure at least two conditions.
+        if (!(element instanceof Perl6IfStatement))
+            return;
+        Perl6IfStatement.IfStructure structure = ((Perl6IfStatement)element).getStructure();
+        Perl6IfStatement.IfPart[] parts = structure.getConditionalParts();
+        if (parts.length < 2)
+            return;
+
+        // Compute a normalization of all the conditions group them on that.
+        Map<String, List<Perl6PsiElement>> duplicated = new HashMap<>();
+        for (Perl6IfStatement.IfPart part : parts) {
+            Perl6PsiElement condition = part.getCondition();
+            if (condition == null)
+                continue;
+            duplicated.computeIfAbsent(normalize(part.isWith(), condition), k -> new ArrayList<>()).add(condition);
+        }
+
+        // If there are any identical conditions, report them.
+        if (duplicated.size() == parts.length) // Fail fast, all distinct
+            return;
+        for (Map.Entry<String, List<Perl6PsiElement>> maybeIdentical : duplicated.entrySet()) {
+            List<Perl6PsiElement> identical = maybeIdentical.getValue();
+            if (identical.size() > 1)
+                for (int i = 1; i < identical.size(); i++)
+                    holder.newAnnotation(HighlightSeverity.WARNING, "An identical condition appears in a previous branch")
+                        .range(identical.get(i))
+                        .create();
+        }
+    }
+
+    private static String normalize(boolean with, Perl6PsiElement condition) {
+        return (with ? "with\0" : "if\0") + normalize(condition);
+    }
+
+    private static String normalize(Perl6PsiElement condition) {
+        // We normalize some binary infix operators.
+        if (condition instanceof Perl6InfixApplication) {
+            String op = ((Perl6InfixApplication)condition).getOperator();
+            PsiElement[] operands = ((Perl6InfixApplication)condition).getOperands();
+            if (op != null && operands.length == 2 &&
+                    operands[0] instanceof Perl6PsiElement && operands[1] instanceof Perl6PsiElement) {
+                // Calculate the normalization fo the operands.
+                String lhsNorm = normalize((Perl6PsiElement)operands[0]);
+                String rhsNorm = normalize((Perl6PsiElement)operands[1]);
+
+                // Normalize any Unicode/ASCII variants.
+                op = UNINORM.getOrDefault(op, op);
+
+                // If it commutes, sort the normalization of the arguments.
+                if (COMMUTERS.contains(op)) {
+                    return lhsNorm.compareTo(rhsNorm) < 0
+                           ? lhsNorm + "\0" + op + "\0" + rhsNorm
+                           : rhsNorm + "\0" + op + "\0" + lhsNorm;
+                }
+
+                // Some things are identical under swapping LHS and RHs.
+                else if (REVERSES.containsKey(op)) {
+                    return rhsNorm + "\0" + REVERSES.get(op) + "\0" + lhsNorm;
+                }
+
+                // Otherwise, just a normal op.
+                else {
+                    return rhsNorm + "\0" + op + "\0" + lhsNorm;
+                }
+            }
+        }
+
+        // Fallback semantics is to go through the children minus whitespace and
+        // nul-separate them.
+        StringJoiner parts = new StringJoiner("\0");
+        PsiElement current = condition.getFirstChild();
+        while ((current = Perl6PsiUtil.skipSpaces(current, true, true)) != null) {
+            parts.add(current instanceof Perl6PsiElement
+                      ? normalize((Perl6PsiElement)current)
+                      : current.getText());
+            current = current.getNextSibling();
+        }
+        return parts.toString();
+    }
+}
