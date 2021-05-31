6e3131179fc26d6e079e49084253ea517e8088ce
vendethiel <vendethiel@hotmail.fr>
2021-05-31 14:50:17 +0200

    Refactor

--- perl6-idea-plugin/src/edument/perl6idea/annotation/fix/AddUseModuleFix.java
+++ perl6-idea-plugin/src/edument/perl6idea/annotation/fix/AddUseModuleFix.java
@@ -11,38 +11,38 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 public class AddUseModuleFix implements IntentionAction {
-  private final String moduleName;
+    private final String moduleName;
 
-  public AddUseModuleFix(String name) {
-    moduleName = name;
-  }
+    public AddUseModuleFix(String name) {
+        moduleName = name;
+    }
 
-  @Nls
-  @NotNull
-  @Override
-  public String getText() {
-    return "Use " + moduleName + " module";
-  }
+    @Nls
+    @NotNull
+    @Override
+    public String getText() {
+        return "Use " + moduleName + " module";
+    }
 
-  @Nls
-  @NotNull
-  @Override
-  public String getFamilyName() {
-    return "Raku";
-  }
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Raku";
+    }
 
-  @Override
-  public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
-    return ModuleUtilCore.findModuleForFile(file) != null;
-  }
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+        return ModuleUtilCore.findModuleForFile(file) != null;
+    }
 
-  @Override
-  public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
-    Perl6UseUtils.addUse(editor, file, moduleName);
-  }
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+        Perl6UseUtils.addUse(editor, file, moduleName, moduleName);
+    }
 
-  @Override
-  public boolean startInWriteAction() {
-    return true;
-  }
+    @Override
+    public boolean startInWriteAction() {
+        return true;
+    }
 }
--- perl6-idea-plugin/src/edument/perl6idea/utils/Perl6UseUtils.java
+++ perl6-idea-plugin/src/edument/perl6idea/utils/Perl6UseUtils.java
@@ -13,37 +13,37 @@ import edument.perl6idea.psi.Perl6StatementList;
 import edument.perl6idea.psi.Perl6UseStatement;
 
 public class Perl6UseUtils {
-  public static boolean usesModule(PsiElement element, String expected) {
-    Perl6PsiScope scope = PsiTreeUtil.getParentOfType(element, Perl6PsiScope.class);
-    int elementTextOffset = element.getTextOffset();
-    while (scope != null) {
-      Perl6StatementList list = PsiTreeUtil.findChildOfType(scope, Perl6StatementList.class);
-      if (list == null) break;
-      Perl6Statement[] stats = PsiTreeUtil.getChildrenOfType(list, Perl6Statement.class);
-      if (stats == null) stats = new Perl6Statement[0];
-      for (Perl6Statement statement : stats) {
-        if (statement.getTextOffset() > elementTextOffset) break;
-        for (PsiElement child : statement.getChildren()) {
-          if (!(child instanceof Perl6UseStatement)) continue;
-          String moduleName = ((Perl6UseStatement)child).getModuleName();
-          if (expected.equals(moduleName)) {
-            return true;
-          }
+    public static boolean usesModule(PsiElement element, String expected) {
+        Perl6PsiScope scope = PsiTreeUtil.getParentOfType(element, Perl6PsiScope.class);
+        int elementTextOffset = element.getTextOffset();
+        while (scope != null) {
+            Perl6StatementList list = PsiTreeUtil.findChildOfType(scope, Perl6StatementList.class);
+            if (list == null) break;
+            Perl6Statement[] stats = PsiTreeUtil.getChildrenOfType(list, Perl6Statement.class);
+            if (stats == null) stats = new Perl6Statement[0];
+            for (Perl6Statement statement : stats) {
+                if (statement.getTextOffset() > elementTextOffset) break;
+                for (PsiElement child : statement.getChildren()) {
+                    if (!(child instanceof Perl6UseStatement)) continue;
+                    String moduleName = ((Perl6UseStatement)child).getModuleName();
+                    if (expected.equals(moduleName)) {
+                        return true;
+                    }
+                }
+            }
+            scope = PsiTreeUtil.getParentOfType(scope, Perl6PsiScope.class);
         }
-      }
-      scope = PsiTreeUtil.getParentOfType(scope, Perl6PsiScope.class);
+
+        return false;
     }
 
-    return false;
-  }
-
-  public static void addUse(Editor editor, PsiFile file, String moduleName) {
-    editor.getDocument().insertString(0, "use " + moduleName + ";\n");
-    Module module = ModuleUtilCore.findModuleForFile(file);
-    assert module != null;
-    Perl6MetaDataComponent metaData = module.getService(Perl6MetaDataComponent.class);
-    if (!metaData.getDepends(true).contains(moduleName)) {
-      metaData.addDepends(moduleName);
+    public static void addUse(Editor editor, PsiFile file, String useName, String moduleName) {
+        editor.getDocument().insertString(0, "use " + useName + ";\n");
+        Module module = ModuleUtilCore.findModuleForFile(file);
+        assert module != null;
+        Perl6MetaDataComponent metaData = module.getService(Perl6MetaDataComponent.class);
+        if (!metaData.getDepends(true).contains(moduleName)) {
+            metaData.addDepends(moduleName);
+        }
     }
-  }
 }
