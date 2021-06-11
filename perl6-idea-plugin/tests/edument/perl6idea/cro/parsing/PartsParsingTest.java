73ef08d91656d757dfc7ce59afef2e9eb0ae018f
Jonathan Worthington <jnthn@jnthn.net>
2021-06-11 16:31:49 +0200

    Add Cro template parts parsing test

---
+++ perl6-idea-plugin/tests/edument/perl6idea/cro/parsing/PartsParsingTest.java
@@ -0,0 +1,7 @@
+package edument.perl6idea.cro.parsing;
+
+public class PartsParsingTest extends CroTemplateParsingTestCase {
+    public PartsParsingTest(String testPath) {
+        super("parts");
+    }
+}
