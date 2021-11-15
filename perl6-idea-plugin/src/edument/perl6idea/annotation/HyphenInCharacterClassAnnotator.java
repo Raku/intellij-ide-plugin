7c2de9cb655a2566f72bcf72cb9bab6f14c149a9
Altai-man <alexander.kiryuhin@gmail.com>
2021-11-15 13:18:47 +0100

    Do not warn about hyphen at the start of character class

--- perl6-idea-plugin/src/edument/perl6idea/annotation/HyphenInCharacterClassAnnotator.java
+++ perl6-idea-plugin/src/edument/perl6idea/annotation/HyphenInCharacterClassAnnotator.java
@@ -1,5 +1,6 @@
 package edument.perl6idea.annotation;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
@@ -16,13 +17,19 @@ public class HyphenInCharacterClassAnnotator implements Annotator {
         if (element instanceof Perl6RegexCClassElem) {
             PsiElement child = element.getFirstChild();
             while (child != null) {
-                if (child.getNode().getElementType() != REGEX_CCLASS_SYNTAX && child.getText().equals("-"))
+                if (child.getNode().getElementType() != REGEX_CCLASS_SYNTAX && child.getText().equals("-")) {
+                    ASTNode prev = child.getNode().getTreePrev();
+                    if (prev != null && prev.getElementType() == REGEX_CCLASS_SYNTAX && prev.getText().equals("[")) {
+                        child = child.getNextSibling();
+                        continue;
+                    }
                     holder.newAnnotation(
                         HighlightSeverity.ERROR,
                         "A hyphen is used in a character class, maybe '..' was intended to denote a range? Otherwise a hyphen should be at the end of the character class.")
                         .range(child)
                         .withFix(new ReplaceHyphenWithRange(child))
                         .create();
+                }
                 child = child.getNextSibling();
                 PsiElement sibling = child == null ? null : child.getNextSibling();
                 if (sibling != null && sibling.getText().equals("]"))
