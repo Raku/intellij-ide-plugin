9a4555b88078aea165c08d21090c9e0643edb369
Jonathan Worthington <jnthn@jnthn.net>
2022-03-29 17:42:53 +0200

    Add no progress analysis for alternations
    And refactor handling of groups/captures to not duplicate code.

--- perl6-idea-plugin/src/edument/perl6idea/psi/Perl6RegexPsiElement.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/Perl6RegexPsiElement.java
@@ -4,4 +4,15 @@ public interface Perl6RegexPsiElement extends Perl6PsiElement {
     default boolean mightMatchZeroWidth() {
         return false;
     }
+
+    default boolean atomsMightMatchZeroWidth(Perl6RegexAtom[] atoms) {
+        // Everything in the sequence of atoms must potentially match nothing.
+        if (atoms != null) {
+            for (Perl6RegexAtom atom : atoms) {
+                if (!atom.mightMatchZeroWidth())
+                    return false;
+            }
+        }
+        return true;
+    }
 }
--- perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6RegexGroupImpl.java
+++ perl6-idea-plugin/src/edument/perl6idea/psi/impl/Perl6RegexGroupImpl.java
@@ -3,6 +3,8 @@ package edument.perl6idea.psi.impl;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import edument.perl6idea.psi.Perl6RegexAtom;
 import edument.perl6idea.psi.Perl6RegexGroup;
 import edument.perl6idea.psi.Perl6RegexPsiElement;
 import org.jetbrains.annotations.NotNull;
@@ -14,20 +16,6 @@ public class Perl6RegexGroupImpl extends ASTWrapperPsiElement implements Perl6Re
 
     @Override
     public boolean mightMatchZeroWidth() {
-        // Everything in the group must potentially match nothing.
-        for (PsiElement child : getChildren()) {
-            // If it's not a known regex element, then can't analyze it.
-            if (!(child instanceof Perl6RegexPsiElement))
-                return false;
-
-            // Otherwise, see if it might match zero width. If we can't be
-            // sure it will, then have to assume it can match something.
-            if (!((Perl6RegexPsiElement)child).mightMatchZeroWidth())
-                return false;
-        }
-
-        // If everything could match zero width, then this is true of the
-        // group as a whole.
-        return true;
+        return atomsMightMatchZeroWidth(PsiTreeUtil.getChildrenOfType(this, Perl6RegexAtom.class));
     }
 }
