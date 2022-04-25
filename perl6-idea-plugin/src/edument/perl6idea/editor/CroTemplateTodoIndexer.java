e21434eedc62d0aaacfebc84f04fa60a7e17d096
Altai-man <alexander.kiryuhin@gmail.com>
2022-04-25 19:44:13 +0200

    Make TODO feature work for Raku and Cro templates
    Previously we had no indexer present, so the TODO items
    in comments were not present in the tool window, so add
    them.

---
+++ perl6-idea-plugin/src/edument/perl6idea/editor/CroTemplateTodoIndexer.java
@@ -0,0 +1,34 @@
+package edument.perl6idea.editor;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.psi.impl.cache.impl.BaseFilterLexer;
+import com.intellij.psi.impl.cache.impl.IdAndToDoScannerBasedOnFilterLexer;
+import com.intellij.psi.impl.cache.impl.OccurrenceConsumer;
+import com.intellij.psi.impl.cache.impl.todo.LexerBasedTodoIndexer;
+import com.intellij.psi.tree.IElementType;
+import edument.perl6idea.cro.template.parsing.CroTemplateLexer;
+import edument.perl6idea.cro.template.parsing.CroTemplateTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CroTemplateTodoIndexer extends LexerBasedTodoIndexer implements IdAndToDoScannerBasedOnFilterLexer {
+    @Override
+    public @NotNull Lexer createLexer(@NotNull OccurrenceConsumer consumer) {
+        return new RakuFilterLexer(consumer);
+    }
+
+    private static class RakuFilterLexer extends BaseFilterLexer {
+        RakuFilterLexer(@NotNull OccurrenceConsumer consumer) {
+            super(new CroTemplateLexer(), consumer);
+        }
+
+        @Override
+        public void advance() {
+            @Nullable IElementType tokenType = myDelegate.getTokenType();
+            if (tokenType == CroTemplateTokenTypes.COMMENT) {
+                advanceTodoItemCountsInToken();
+            }
+            myDelegate.advance();
+        }
+    }
+}
