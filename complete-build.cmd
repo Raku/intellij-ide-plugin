eab01d5a418bfef1cde1eb22567a9d5c5b6cd85b
Altai-man <alexander.kiryuhin@gmail.com>
2022-05-25 19:31:24 +0200

    Start work on the complete build

---
+++ complete-build.cmd
@@ -0,0 +1,11 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+set -eux
+exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" edument.perl6.comma.build CommaCompleteInstallersBuildTarget
+:CMDSCRIPT
+
+call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* edument.perl6.comma.build CommaCompleteInstallersBuildTarget
+EXIT /B %ERRORLEVEL%
