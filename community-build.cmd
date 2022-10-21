7680a24e48dd1310fad2f7469779c042081aa06c
Jonathan Worthington <jnthn@jnthn.net>
2022-10-21 18:23:38 +0200

    Fix communnity build paths

--- community-build.cmd
+++ community-build.cmd
@@ -4,7 +4,8 @@ GOTO :CMDSCRIPT
 ::CMDLITERAL
 
 set -eux
-exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" edument.perl6.comma.build CommaCommunityInstallersBuildTarget
+root="$(cd "$(dirname "$0")/.."; pwd)"
+exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" intellij.idea.community.build OpenSourceCommunityInstallersBuildTarget
 :CMDSCRIPT
 
 call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* edument.perl6.comma.build CommaCommunityInstallersBuildTarget
