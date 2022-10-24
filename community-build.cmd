c57379c406f3adadd0ce8c0c58f2e91d339cf77e
Jonathan Worthington <jnthn@jnthn.net>
2022-10-24 17:50:28 +0200

    Fix copy-pasta

--- community-build.cmd
+++ community-build.cmd
@@ -5,7 +5,7 @@ GOTO :CMDSCRIPT
 
 set -eux
 root="$(cd "$(dirname "$0")/.."; pwd)"
-exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" intellij.idea.community.build OpenSourceCommunityInstallersBuildTarget
+exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" edument.perl6.comma.build CommaCommunityInstallersBuildTarget
 :CMDSCRIPT
 
 call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* edument.perl6.comma.build CommaCommunityInstallersBuildTarget
