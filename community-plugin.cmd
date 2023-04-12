b2e08bfb752ae34f0ff29daf0451cf2b4c1d2e54
Altai-man <alexander.kiryuhin@gmail.com>
2023-04-12 18:12:44 +0200

    Call correct build target for community plugin

--- community-plugin.cmd
+++ community-plugin.cmd
@@ -5,7 +5,7 @@ GOTO :CMDSCRIPT
 
 set -eux
 root="$(cd "$(dirname "$0")/.."; pwd)"
-exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" edument.perl6.comma.build CommaCommunityInstallersBuildTarget
+exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" edument.perl6.comma.build CommaCommunityPluginBuildTarget
 :CMDSCRIPT
 
 call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* edument.perl6.comma.build CommaCommunityPluginBuildTarget
