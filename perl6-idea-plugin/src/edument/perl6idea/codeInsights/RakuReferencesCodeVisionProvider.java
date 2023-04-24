df06695542401e1982e22212c2ca37138830cf0d
Altai-man <alexander.kiryuhin@gmail.com>
2023-04-24 19:15:10 +0200

    Add packages and scope support for refs code vision provider

--- perl6-idea-plugin/src/edument/perl6idea/codeInsights/RakuReferencesCodeVisionProvider.java
+++ perl6-idea-plugin/src/edument/perl6idea/codeInsights/RakuReferencesCodeVisionProvider.java
@@ -6,8 +6,11 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
 import edument.perl6idea.Perl6Language;
+import edument.perl6idea.psi.Perl6PackageDecl;
 import edument.perl6idea.psi.Perl6RoutineDecl;
+import edument.perl6idea.psi.Perl6ScopedDecl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,13 +26,23 @@ public class RakuReferencesCodeVisionProvider extends ReferencesCodeVisionProvid
 
     @Override
     public boolean acceptsElement(@NotNull PsiElement element) {
-        return element instanceof Perl6RoutineDecl;
+        return element instanceof Perl6RoutineDecl || element instanceof Perl6PackageDecl ||
+               element instanceof Perl6ScopedDecl;
     }
 
     @Nullable
     @Override
     public String getHint(@NotNull PsiElement element, @NotNull PsiFile file) {
-        Collection<PsiReference> finds = ReferencesSearch.search(element).findAll();
+        PsiElement elementToRefSearch = element;
+        if (element.getParent() instanceof Perl6ScopedDecl)
+            return null;
+        if (element instanceof Perl6ScopedDecl) {
+            elementToRefSearch = PsiTreeUtil.getChildOfAnyType(element, Perl6PackageDecl.class, Perl6RoutineDecl.class);
+            if (elementToRefSearch == null)
+                return null;
+        }
+
+        Collection<PsiReference> finds = ReferencesSearch.search(elementToRefSearch).findAll();
         return switch (finds.size()) {
             case 0 -> "No usages";
             case 1 -> "1 usage";
